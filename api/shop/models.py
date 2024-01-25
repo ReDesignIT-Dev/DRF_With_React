3659db8279b2e5bc95bd15d7c11a2d104cec69f6
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-25 18:38:15 +0100

    change DISCOUNT_RATE to Decimal. There were problems trying to multiply float and decimal

--- api/shop/models.py
+++ api/shop/models.py
@@ -1,9 +1,10 @@
+from decimal import Decimal
 from django.utils import timezone
 from django.db import models
 
 
 class Product(models.Model):
-    DISCOUNT_RATE = 0.10
+    DISCOUNT_RATE = Decimal(0.10)
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -11,7 +12,7 @@ class Product(models.Model):
     price = models.FloatField()
     sale_start = models.DateTimeField(blank=True, null=True, default=None)
     sale_end = models.DateTimeField(blank=True, null=True, default=None)
-    photo = models.ImageField(blank=True, null=True, default=None, upload_to='products')
+    photo = models.ImageField(blank=True, null=True, default=None, upload_to="products")
 
     def is_on_sale(self):
         now = timezone.now()
@@ -26,7 +27,8 @@ class Product(models.Model):
 
     def current_price(self):
         if self.is_on_sale():
-            discounted_price = self.price * (1 - self.DISCOUNT_RATE)
+            print(f'price type -> {type(self.price)}, discount type -> {type(self.DISCOUNT_RATE)}')
+            discounted_price = Decimal(self.price) * (1 - self.DISCOUNT_RATE)
             return round(discounted_price, 2)
         return self.get_rounded_price()
 
@@ -54,19 +56,25 @@ class ShoppingCart(models.Model):
         return round(self.subtotal() * self.taxes(), 2)
 
     def __repr__(self):
-        name = self.name or '[Guest]'
-        address = self.address or '[No Address]'
+        name = self.name or "[Guest]"
+        address = self.address or "[No Address]"
         return '<ShoppingCart object ({}) "{}" "{}">'.format(self.id, name, address)
 
 
 class ShoppingCartItem(models.Model):
-    shopping_cart = models.ForeignKey(ShoppingCart, related_name='items', related_query_name='item',
-                                      on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='+', on_delete=models.CASCADE)
+    shopping_cart = models.ForeignKey(
+        ShoppingCart,
+        related_name="items",
+        related_query_name="item",
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(Product, related_name="+", on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     def total(self):
         return round(self.quantity * self.product.current_price())
 
     def __repr__(self):
-        return '<ShoppingCartItem object ({}) {}x "{}">'.format(self.id, self.quantity, self.product.name)
+        return '<ShoppingCartItem object ({}) {}x "{}">'.format(
+            self.id, self.quantity, self.product.name
+        )
