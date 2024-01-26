400c013574b51c8ff7739f6a975277f04f7c83f1
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-26 17:51:23 +0100

    add urls and serializer to work with views

--- api/shop/serializers.py
+++ api/shop/serializers.py
@@ -1,15 +1,59 @@
 from rest_framework import serializers
+from .models import Product, ShoppingCartItem
 
-from .models import Product
 
+class CartItemSerializer(serializers.ModelSerializer):
+    quantity = serializers.IntegerField(min_value=1, max_value=100)
+    class Meta:
+        model = ShoppingCartItem
+        fields = ('product', 'quantity')
 
 class ProductSerializer(serializers.ModelSerializer):
+    is_on_sale = serializers.BooleanField(read_only=True)
+    current_price = serializers.FloatField(read_only=True)
+    description = serializers.CharField(min_length=2, max_length=200) #override the model
+    cart_items = serializers.SerializerMethodField()
+    #price = serializers.FloatField(min_value=1.00, max_value=100000)
+    price = serializers.DecimalField(min_value=1.00, max_value=100000, max_digits=None, decimal_places=2)
+    sale_start = serializers.DateTimeField(
+        required=False,
+        input_formats=['%Y-%m-%d %H:%M'], 
+        format='%Y-%m-%d %H:%M', 
+        allow_null=True,
+        help_text='Accepted format is "2000-01-01 12:00"',
+        style={'input_type': 'text', 'placeholder': '2000-12-12 12:00'},
+    )
+    sale_end = serializers.DateTimeField(
+        required=False,
+        input_formats=['%Y-%m-%d %H:%M'], 
+        format='%Y-%m-%d %H:%M', 
+        allow_null=True,
+        help_text='Accepted format is "2000-01-01 12:00"',
+        style={'input_type': 'text', 'placeholder': '2000-12-12 12:00'},
+    )
+    photo = serializers.ImageField(default=None)
+    warranty = serializers.FileField(write_only=True, default=None)
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end')
+        fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end', 'is_on_sale', 'current_price', 'cart_items', 'photo', 'warranty')
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['is_one_sale'] = instance.is_on_sale()
-        data['current_price'] = instance.current_price()
-        return data
+    def get_cart_items(self, instance):
+        items = ShoppingCartItem.objects.filter(product=instance)
+        return CartItemSerializer(items, many=True).data
+    
+    def update(self, instance, validated_data):
+        if validated_data.get('warranty', None):
+            instance.description += '\n\nWarranty Information:\n'
+            instance.description += b'; '.join(
+                validated_data['warranty'].readlines()
+            ).decode()
+        return instance
+    
+    def create(self, validated_data):
+        validated_data.pop('warranty')
+        return Product.objects.create(**validated_data)
+    
+
+class ProductStatSerializer(serializers.Serializer):
+    stats = serializers.DictField(child=serializers.ListField(child=serializers.IntegerField()))
--- api/shop/urls.py
+++ api/shop/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, ProductCreate, ProductRetrieveUpdateDestroy, ProductStats
 
 
 urlpatterns = [
     path('products/', ProductList.as_view(), name='products'),
+    path('products/create', ProductCreate.as_view(), name='create_product'),
+    path('products/<int:id>/', ProductRetrieveUpdateDestroy.as_view(), name='delete_product'),
+    path('products/<int:id>/stats', ProductStats.as_view(), name='product_stats')
 ]
