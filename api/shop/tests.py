6df655fdc6941b9f46eb313a104fb6d88bed3e0e
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-28 19:33:42 +0100

    destroy test (TODO make creation of new product in separate method)

--- api/shop/tests.py
+++ api/shop/tests.py
@@ -23,3 +23,25 @@ class ProductCreateTestCase(APITestCase):
             self.assertEqual(
                 response.data["current_price"], float(product_attrs["price"])
             )
+
+class ProductDestroyTestCase(APITestCase):
+    def test_delete_product(self):
+        
+        product_attrs = {
+            "name": "New Product",
+            "description": "Awesome product",
+            "price": "123.45",
+        }
+        response = self.client.post("/products/create", product_attrs)
+        initial_product_count = Product.objects.count()
+        print(f'initial count = {initial_product_count}')
+        product_id = Product.objects.first().id
+        self.client.delete('/products/{}/'.format(product_id))
+        self.assertEqual(
+            Product.objects.count(),
+            initial_product_count -1,
+        )
+        self.assertRaises(
+            Product.DoesNotExist,
+            Product.objects.get, id=product_id,
+        )
