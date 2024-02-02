d94ebd7a082a7f948edf99a83535fa3856a85662
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-02-02 19:05:48 +0100

    give better name to urls

--- api/shop/urls.py
+++ api/shop/urls.py
@@ -5,6 +5,6 @@ from .views import ProductList, ProductCreate, ProductRetrieveUpdateDestroy, Pro
 urlpatterns = [
     path('products/', ProductList.as_view(), name='products'),
     path('products/create', ProductCreate.as_view(), name='create_product'),
-    path('products/<int:id>/', ProductRetrieveUpdateDestroy.as_view(), name='delete_product'),
+    path('products/<int:id>/', ProductRetrieveUpdateDestroy.as_view(), name='update_delete_product'),
     path('products/<int:id>/stats', ProductStats.as_view(), name='product_stats')
 ]
