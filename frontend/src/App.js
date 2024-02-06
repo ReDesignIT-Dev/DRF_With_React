154cb6b1588fb496d29f1b240aa8e4fb6623e94d
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-02-06 20:55:39 +0100

    Random tests how to work with react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [name, setName] = useState("Stefan");
+  const [surname, setSurname] = useState("Stefanowski");
+
+  useEffect(() => {
+    console.log(`Now it's ${name} & ${surname}`);
+  }, [name, surname]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload..
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> Now its the {name} {surname}</h1>
+      <button onClick={() => setName("Boguś")}>
+        Boguś inside
+      </button>
+      
+      <button onClick={() => setSurname("Wacławowski")}>
+        Wacławowski inside
+      </button>
     </div>
+
   );
+
 }
 
 export default App;
