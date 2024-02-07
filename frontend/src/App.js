a952ed8b03d843d18cb6a044a9e077ec7f0d5967
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-02-07 18:46:54 +0100

    update to use bootstrap and mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Stack from '@mui/material/Stack';
 
 function App() {
 
@@ -15,13 +18,20 @@ function App() {
     <div className="App">
 
       <h1> Now its the {name} {surname}</h1>
-      <button onClick={() => setName("Boguś")}>
-        Boguś inside
-      </button>
-      
-      <button onClick={() => setSurname("Wacławowski")}>
-        Wacławowski inside
-      </button>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Button variant="primary" onClick={() => setName("Boguś")}>
+          Boguś inside
+        </Button>
+
+        <Button onClick={() => setSurname("Wacławowski")}>
+          Wacławowski inside
+        </Button>
+      </Stack>
     </div>
 
   );
