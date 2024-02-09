a3474632ca267476423deec801f640548ef07921
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-02-09 23:46:00 +0100

    clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,6 @@ function App() {
       onChange={(event) => setTitle(event.target.value)}
       type="text"
       placeholder='color title...'
-      
       />
       <input value={color} type="color" onChange={(event) => setColor(event.target.value)} />
       <button> ADD </button>
