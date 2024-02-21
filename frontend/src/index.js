867be2d56dff6cf52085f4c205eaa35dd2a7604b
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-02-21 16:55:02 +0100

    delete force refresh for the time being. Not used at the time

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,7 @@ import Footer from "components/Footer";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter forceRefresh={true}>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
