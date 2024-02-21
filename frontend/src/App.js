91484ba168ab68bdc7c510148df8e970f05810e8
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-02-21 17:15:00 +0100

    Move the structure to the App as people like

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from 'pages/Home';
+import About from "pages/About";
+import Contact from "pages/Contact";
+import Header from "components/Header";
+import Footer from "components/Footer";
 
 function App() {
-  return (<>
-  
-  </>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from 'pages/Home';
-import About from "pages/About";
-import Contact from "pages/Contact";
-import Header from "components/Header";
-import Footer from "components/Footer";
+import App from "App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
