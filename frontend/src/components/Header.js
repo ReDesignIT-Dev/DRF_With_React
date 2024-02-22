4eab6f38901dc37acb66cac476f84a14d8ba84b3
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-02-22 20:53:28 +0100

    add styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LoginFormPop from "./LoginFormPop";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
+import "./Header.scss";
 
 function Header() {
 
@@ -13,11 +14,15 @@ function Header() {
 
   return (
     <div className="header">
-      <Link to="/"> Home </Link>
-      <Link to="/contact"> Contact </Link>
-      <Link to="/about"> About </Link>
-      <Link to="/login"> Login </Link>
-      <Link to="/about/history"> History </Link>
+      <div className="top-header-info"></div>
+      <div className="header-nav">
+        <div className="nav-element"><Link to="/"> Home </Link></div>
+        <div className="nav-element"><Link to="/contact"> Contact </Link></div>
+        <div className="nav-element"><Link to="/about"> About </Link></div>
+        <div className="nav-element"><Link to="/login"> Login </Link></div>
+        <div className="nav-element"><Link to="/about/history"> History </Link></div>
+      </div>
+
       <Navbar handleLoginClick={handleLoginClick} />
       <LoginFormPop isShowLogin={isShowLogin} />
     </div>
