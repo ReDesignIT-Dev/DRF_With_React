dc59be7fbe38be1eea52455c3a8e90847a1acee3
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-02-26 19:18:03 +0100

    work on header style

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import LoginFormPop from "./LoginFormPop";
 import SignInButton from "./SignInButton";
+import SearchBox from "./SearchBox";
 import { Link } from "react-router-dom";
 import "./Header.scss";
+import logo from "images/racoon-logo.png";
 
 function Header() {
 
@@ -13,15 +15,20 @@ function Header() {
   };
 
   return (
-    <div className="header">
-      <div className="top-header-info"></div>
+    <div className="header d-flex flex-column py-2">
+      <div className="top-header d-flex flex-row gap-2 justify-content-center align-items-center">
+        <div className="header-logo"><Link to="/"> <img src={logo} alt="LOGO" /></Link></div>
+        <div className="header-searchbar"><SearchBox/></div>
+        <div className="header-signin"><SignInButton handleLoginClick={handleLoginClick} /></div>
+      </div>
+
       <div className="header-nav d-flex flex-row gap-2">
         <div className="nav-element"><Link to="/"> Home </Link></div>
         <div className="nav-element"><Link to="/contact"> Contact </Link></div>
         <div className="nav-element"><Link to="/about"> About </Link></div>
         <div className="nav-element"><Link to="/login"> Login </Link></div>
         <div className="nav-element"><Link to="/about/history"> History </Link></div>
-        <SignInButton handleLoginClick={handleLoginClick} />
+        
       </div>
       <LoginFormPop isShowLogin={isShowLogin} handleXClick={handleLoginClick} />
     </div>
