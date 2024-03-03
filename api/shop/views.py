7ac2bc33217d1275935d2a9932843bd85dc3fc01
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-03-03 09:10:31 +0100

    permission update to get from home

--- api/shop/views.py
+++ api/shop/views.py
@@ -1,97 +1,119 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    GenericAPIView,
+)
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import ProductSerializer, ProductStatSerializer
 from .models import Product
 from rest_framework.views import APIView
 from rest_framework import status
 
+
 class HomeView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request, format=None):
-        return Response({'message': 'Hello, Home View! Yeah'}, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "Hello, Home View! Yeah"}, status=status.HTTP_200_OK
+        )
+
 
 class ProductsPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 100
 
+
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
-    filterset_fields = ('id', )
-    search_fields = ('name', 'description')
+    filterset_fields = ("id",)
+    search_fields = ("name", "description")
     pagination_class = ProductsPagination
 
     def get_queryset(self):
-        on_sale = self.request.query_params.get('on_sale', None)
+        on_sale = self.request.query_params.get("on_sale", None)
         if on_sale is None:
             return super().get_queryset()
         queryset = Product.objects.all()
-        if on_sale.lower() == 'true':
+        if on_sale.lower() == "true":
             from django.utils import timezone
+
             now = timezone.now()
             return queryset.filter(
                 sale_start__lte=now,
                 sale_end__gte=now,
             )
         return queryset
-    
+
 
 class ProductCreate(CreateAPIView):
     serializer_class = ProductSerializer
 
     def create(self, request, *args, **kwargs):
         try:
-            price = request.data.get('price')
+            price = request.data.get("price")
             if price is not None and float(price) <= 0.0:
-                raise ValidationError({'price': 'Must be above 0'})
+                raise ValidationError({"price": "Must be above 0"})
         except:
-            raise ValidationError({'price': 'A valid number is required'})
+            raise ValidationError({"price": "A valid number is required"})
         return super().create(request, *args, **kwargs)
-    
+
+
 class ProductRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-    lookup_field = 'id'
+    lookup_field = "id"
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-    
+
     def delete(self, request, *args, **kwargs):
-        product_id = request.data.get('id')
+        product_id = request.data.get("id")
         response = super().delete(request, *args, **kwargs)
         if response.status_code == 204:
             from django.core.cache import cache
-            cache.delete('product_data_{}'.format(product_id))
+
+            cache.delete("product_data_{}".format(product_id))
         return response
-    
+
     def update(self, request, *args, **kwargs):
         response = super().update(request, *args, **kwargs)
-        product = Product.objects.get(id=response.data['id'])
-        product.save()  
+        product = Product.objects.get(id=response.data["id"])
+        product.save()
         if response.status_code == 200:
             from django.core.cache import cache
+
             product = response.data
-            cache.set('product_data_{}'.format(product['id']), {
-                'name': product['name'],
-                'description': product['description'],
-                'price': product['price']
-            })
-            
+            cache.set(
+                "product_data_{}".format(product["id"]),
+                {
+                    "name": product["name"],
+                    "description": product["description"],
+                    "price": product["price"],
+                },
+            )
+
         return response
-    
+
 
 class ProductStats(GenericAPIView):
-    lookup_field = 'id'
+    lookup_field = "id"
     serializer_class = ProductStatSerializer
     queryset = Product.objects.all()
 
     def get(self, request, format=None, id=None):
         obj = self.get_object()
-        serializer = ProductStatSerializer({
-            'stats': {
-                '2019-01-01': [5,10,15],
-                '2019-01-02': [20,1,1],
+        serializer = ProductStatSerializer(
+            {
+                "stats": {
+                    "2019-01-01": [5, 10, 15],
+                    "2019-01-02": [20, 1, 1],
+                }
             }
-        })
+        )
         return Response(serializer.data)
