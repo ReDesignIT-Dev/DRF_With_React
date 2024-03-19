b810f0f0ccc9d047b928a254b89a8e5e55e62782
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-03-19 21:32:23 +0100

    connect validation logic and reformat with prettier

--- frontend/src/components/LoginForm/LoginFormPop.js
+++ frontend/src/components/LoginForm/LoginFormPop.js
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./LoginFormPop.scss";
 import "react-bootstrap";
 import { postData } from "services/apiRequests";
-import {Icon} from "react-icons-kit";
-import {eye, eyeOff} from "react-icons-kit/feather";
+import { Icon } from "react-icons-kit";
+import { eye, eyeOff } from "react-icons-kit/feather";
 import Input from "./Input";
 import ErrorLabel from "./ErrorLabel";
-import { isEmpty, isEmailValid, isPasswordValid, isTheSamePassword } from "utils/validation";
+import {
+  isEmpty,
+  isEmailValid,
+  isPasswordValid,
+  isTheSamePassword,
+  isLengthValid,
+  isUppercaseValid,
+  isLowercaseValid,
+  isDigitValid,
+  isSpecialCharValid,
+} from "utils/validation";
 
 const LoginFormPop = ({ isShowLogin, handleXClick }) => {
-
   const [message, setMessage] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [icon, setIcon] = useState(eyeOff);
@@ -24,7 +33,6 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
   const [signupPassword, setSignupPassword] = useState("");
   const [signupRepeatPassword, setSignupRepeatPassword] = useState("");
 
-
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
@@ -36,10 +44,10 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
   // detect if in login or signup mode
   const [state, setState] = useState("login");
 
-  const passwordValidationResult = isPasswordValid(signupPassword);
+  const passwordValidationResult = isPasswordValid(password);
 
   const toggleState = () => {
-    state === 'login' ? setState('signup') : setState('login');
+    state === "login" ? setState("signup") : setState("login");
     setSignupPassword("");
   };
 
@@ -54,15 +62,17 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
     setPasswordError("");
     setSignupEmailError("");
     setSignupRepeatPasswordError("");
-  }
-
-
+  };
 
   const onSubmitClick = () => {
     clearErrors();
-    if (state === "login") { handleLogin(); }
-    else if (state === "signup") { handleSignup(); }
-    else { handleInvalidState(); }
+    if (state === "login") {
+      handleLogin();
+    } else if (state === "signup") {
+      handleSignup();
+    } else {
+      handleInvalidState();
+    }
   };
 
   useEffect(() => {
@@ -71,10 +81,9 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
     }
   }, [message]);
 
-
   async function registerUser(userData) {
     try {
-      const response = await postData('register/', userData);
+      const response = await postData("register/", userData);
       setMessage(response.message);
     } catch (error) {
       console.log(error);
@@ -83,7 +92,7 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
 
   async function loginUser(userData) {
     try {
-      const response = await postData('login/', userData);
+      const response = await postData("login/", userData);
       setMessage(response.message);
     } catch (error) {
       console.log(error);
@@ -106,7 +115,7 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
       return;
     }
 
-    console.log('Form submitted with email:', email, 'and password:', password);
+    console.log("Form submitted with email:", email, "and password:", password);
 
     const userData = {
       email: email,
@@ -117,7 +126,6 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
   };
 
   const handleSignup = () => {
-
     if (isEmpty(signupName)) {
       setSignupNameError("Please enter username");
       return;
@@ -148,14 +156,12 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
       return;
     }
 
-
-
     if (!isTheSamePassword(signupPassword, signupRepeatPassword)) {
       setSignupRepeatPasswordError("Passwords do not match");
       return;
     }
 
-    console.log('REGISTER with data:', signupEmail, 'and password:', signupPassword, 'and name:', signupName);
+    console.log("REGISTER with data:", signupEmail, "and password:", signupPassword, "and name:", signupName);
 
     const userData = {
       username: signupName,
@@ -164,7 +170,6 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
     };
 
     registerUser(userData);
-
   };
 
   const handleInvalidState = () => {
@@ -172,46 +177,77 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
     //TODO ADD POPUP WITH ERROR
   };
 
-
   const renderLoginFormFields = () => (
     <div className="form-group form-group--login">
-      <Input type="text" id="email" label="email" value={email} disabled={state === 'signup'} onChange={(ev) => setEmail(ev.target.value)} />
+      <Input type="text" id="email" label="email" value={email} disabled={state === "signup"} onChange={(ev) => setEmail(ev.target.value)} />
       <ErrorLabel error={emailError} state={state} groupState="login" />
-      <Input  type={showPassword ? 'text' : 'password'} id="password" label="password" value={password} disabled={state === 'signup'} onChange={(ev) => setPassword(ev.target.value)} />
+      <Input
+        type={showPassword ? "text" : "password"}
+        id="password"
+        label="password"
+        value={password}
+        disabled={state === "signup"}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
       <span class="flex justify-around items-center" onClick={togglePasswordVisibility}>
-                  <Icon class="absolute mr-10" icon={icon} size={25}/>
-              </span>
+        <Icon class="absolute mr-10" icon={icon} size={25} />
+      </span>
       <ErrorLabel error={passwordError} state={state} groupState="login" />
     </div>
   );
 
   const renderSignupFormFields = () => (
     <div className="form-group form-group--signup">
-      <Input type="text" id="username" label="username" value={signupName} disabled={state === 'login'} onChange={(ev) => setSignupName(ev.target.value)} />
+      <Input
+        type="text"
+        id="username"
+        label="username"
+        value={signupName}
+        disabled={state === "login"}
+        onChange={(ev) => setSignupName(ev.target.value)}
+      />
       <ErrorLabel error={signupNameError} state={state} groupState="signup" />
-      <Input type="text" id="signupEmail" label="email" value={signupEmail} disabled={state === 'login'} onChange={(ev) => setSignupEmail(ev.target.value)} />
+      <Input
+        type="text"
+        id="signupEmail"
+        label="email"
+        value={signupEmail}
+        disabled={state === "login"}
+        onChange={(ev) => setSignupEmail(ev.target.value)}
+      />
       <ErrorLabel error={signupEmailError} state={state} groupState="signup" />
-      <Input type="password" id="createpassword" label="password" disabled={state === 'login'} onChange={(ev) => setSignupPassword(ev.target.value)} />
+      <Input
+        type="password"
+        id="createpassword"
+        label="password"
+        disabled={state === "login"}
+        onChange={(ev) => setSignupPassword(ev.target.value)}
+      />
       <div className="password-validation d-flex flex-column align-items-center">
-      <div className={passwordValidationResult.isLengthValid ? 'valid' : 'invalid'}>Minimum length - 8 characters</div>
-    <div className={passwordValidationResult.isUppercaseValid ? 'valid' : 'invalid'}>At least one uppercase letter</div>
-    <div className={passwordValidationResult.isLowercaseValid ? 'valid' : 'invalid'}>At least one lowercase letter</div>
-    <div className={passwordValidationResult.isDigitValid ? 'valid' : 'invalid'}>At least one numeric digit</div>
-    <div className={passwordValidationResult.isSpecialCharValid ? 'valid' : 'invalid'}>At least one special character</div>
+        <div className={isLengthValid(signupPassword) ? "valid" : "invalid"}>Minimum length - 8 characters</div>
+        <div className={isUppercaseValid(signupPassword) ? "valid" : "invalid"}>At least one uppercase letter</div>
+        <div className={isLowercaseValid(signupPassword) ? "valid" : "invalid"}>At least one lowercase letter</div>
+        <div className={isDigitValid(signupPassword) ? "valid" : "invalid"}>At least one numeric digit</div>
+        <div className={isSpecialCharValid(signupPassword) ? "valid" : "invalid"}>At least one special character</div>
       </div>
-      <Input type="password" id="repeatpassword" label="repeat password" disabled={state === 'login'}
+      <Input
+        type="password"
+        id="repeatpassword"
+        label="repeat password"
+        disabled={state === "login"}
         onChange={(ev) => {
           setSignupRepeatPassword(ev.target.value);
           setSignupRepeatPasswordError("");
-        }} />
+        }}
+      />
       <ErrorLabel error={signupPasswordRepeatError} state={state} groupState="signup" />
     </div>
   );
 
   const renderFormFields = () => {
-    if (state === 'login') {
+    if (state === "login") {
       return renderLoginFormFields();
-    } else if (state === 'signup') {
+    } else if (state === "signup") {
       return renderSignupFormFields();
     } else {
       return null;
@@ -221,45 +257,52 @@ const LoginFormPop = ({ isShowLogin, handleXClick }) => {
   return (
     <div className={`${isShowLogin ? "active " : ""} show login-form`}>
       <div className={`form-box solid app--is-${state} d-flex flex-column justify-content-center`}>
-
         <div className="d-flex justify-content-end close-button-div">
-          <button className="close-button" onClick={handleXClick}> X </button>
+          <button className="close-button" onClick={handleXClick}>
+            {" "}
+            X{" "}
+          </button>
         </div>
-        <div className={`form-block-wrapper form-block-wrapper--is-${state}`} ></div>
+        <div className={`form-block-wrapper form-block-wrapper--is-${state}`}></div>
         <section className={`form-block form-block--is-${state}`}>
           <header className="form-block__header d-flex flex-column">
-            <h1 className="text-center">{state === 'login' ? 'Welcome back!' : 'Sign up'}</h1>
+            <h1 className="text-center">{state === "login" ? "Welcome back!" : "Sign up"}</h1>
             <div className="form-block__toggle-block d-flex flex-row">
               <div className="toggle-text">
-                <span className="text-nowrap d-flex align-items-center">{state === 'login' ? 'Don\'t' : 'Already'} have an account? Click here &#8594;</span>
+                <span className="text-nowrap d-flex align-items-center">
+                  {state === "login" ? "Don't" : "Already"} have an account? Click here &#8594;
+                </span>
               </div>
               <div className="toggle-slider">
-                <input id="form-toggler" type="checkbox" onClick={() => {
-                  toggleState();
-                  clearErrors();
-                }} />
+                <input
+                  id="form-toggler"
+                  type="checkbox"
+                  onClick={() => {
+                    toggleState();
+                    clearErrors();
+                  }}
+                />
                 <label className="flex-shrink-1" htmlFor="form-toggler"></label>
               </div>
             </div>
           </header>
 
-          <form className="login-signup-form" onSubmit={(e) => {
-            e.preventDefault();
-            onSubmitClick();
-          }}>
-            <div className="form-block__input-wrapper">
-              {renderFormFields()}
-            </div>
-            <button className="button button--primary d-flex align-items-center justify-content-center p-3 mt-4 mx-auto" type="submit">{state === 'login' ? 'Log In' : 'Sign Up'}</button>
+          <form
+            className="login-signup-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSubmitClick();
+            }}
+          >
+            <div className="form-block__input-wrapper">{renderFormFields()}</div>
+            <button className="button button--primary d-flex align-items-center justify-content-center p-3 mt-4 mx-auto" type="submit">
+              {state === "login" ? "Log In" : "Sign Up"}
+            </button>
           </form>
         </section>
       </div>
     </div>
   );
 };
-
-
-
-
 
 export default LoginFormPop;
