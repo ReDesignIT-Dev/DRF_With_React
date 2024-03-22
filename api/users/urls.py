3ebc54fbb64032db6a2c1ac676203264c20cd400
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-03-22 19:05:49 +0100

    add urls for registration

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='api-register'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
+    path('accounts/', include('rest_registration.api.urls'))
     # Add other API endpoints as needed
 ]
