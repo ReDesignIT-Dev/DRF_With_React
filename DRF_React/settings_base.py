23c043c0049596822377cf9c116f4e0847190e5f
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-04-04 20:49:47 +0200

    delete doubled

--- DRF_React/settings_base.py
+++ DRF_React/settings_base.py
@@ -24,7 +24,6 @@ FRONTEND_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'frontend', 'build'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv('DJANGO_SECRET_KEY')
-TIME_ZONE = 'UTC'
 
 # Application definition
 
