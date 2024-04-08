4fbb9ad556a713d28549c82f99312fc5d21000c9
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-04-08 17:46:35 +0200

    delete unused

--- api/users/views.py
+++ api/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, status, exceptions
+from rest_framework import generics, permissions, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
