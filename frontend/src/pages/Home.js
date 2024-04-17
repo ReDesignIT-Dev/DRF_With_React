14a1bacd31d5a7c3298371d0f42b108b5dea8977
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-04-17 21:28:24 +0200

    delete unused

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,29 +1,26 @@
 import { getData } from 'services/apiRequests';
 import { useState, useEffect } from 'react';
-
+import SignupSuccessfulPop from "../components/LoginForm/SignupSuccessfulPop";
 export default function Home() {
   const [message, setMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
 
-  useEffect(() => {
+  const handleTestClick = () => {
+    // Update state to show the popup
+    setShowPopup(true);
+    // Set the message here if needed
+    console.log("test 1");
+  };
 
-    async function fetchData() {
-      try {
-        const response = await getData('');
-        setMessage(response.message);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetchData();
-  }, []);
   return (
     <div>
       <h1> Main page </h1>
-
+ 
       <p>{message}</p>
 
       <h1>Welcome to Our Shop</h1>
+          <button onClick={handleTestClick}>Test button</button>
+      {showPopup && <SignupSuccessfulPop message={message} />}
 
     </div>
   );
