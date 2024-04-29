344e5fefdea0f65184a803bfb852676f809814ab
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-04-29 18:40:52 +0200

    add urls for product category

--- api/shop/urls.py
+++ api/shop/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import ProductList, ProductCreate, ProductRetrieveUpdateDestroy, ProductStats, HomeView
-
+from .views import ProductList, ProductCreate, ProductRetrieveUpdateDestroy, HomeView, ProductView, CategoriesView, \
+    CategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/', ProductList.as_view(), name='products'),
-    path('products/create', ProductCreate.as_view(), name='create_product'),
-    path('products/<int:id>/', ProductRetrieveUpdateDestroy.as_view(), name='update_delete_product'),
-    path('products/<int:id>/stats', ProductStats.as_view(), name='product_stats')
+    path('products/create', ProductCreate.as_view(), name='product_create'),
+    path('products/<slug:slug>', ProductView.as_view(), name='product'),
+    path('products/<slug:slug>/edit', ProductRetrieveUpdateDestroy.as_view(), name='product_update_delete'),
+    path('categories/', CategoriesView.as_view(), name='categories'),
+    path('category/', CategoryView.as_view(), name='category')
 ]
