e163b9a4beccf4ff8aefe1c8bb068afc90b3150c
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-04 18:49:10 +0200

    delete unused and change frontendroot to mediaroot on backend to serve images

--- DRF_React/urls.py
+++ DRF_React/urls.py
@@ -21,9 +21,8 @@ from django.conf.urls.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api-auth/', include('rest_framework.urls')),
     path('', include('api.shop.urls')),
     path('', include('api.users.urls')),
     re_path(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2')),
-    re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_ROOT})
+    re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
