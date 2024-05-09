44a27cfa4489a6d23784cdbdb27a6b92355f8710
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-09 16:16:51 +0200

    user simple serializer as the defaulregisteruserserializer dont work well with additional fields

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,40 +1,34 @@
-from rest_framework import serializers
-from rest_registration.api.serializers import DefaultRegisterUserSerializer, DefaultLoginSerializer
+from rest_framework.serializers import Serializer, ModelSerializer, CharField, ValidationError
 from .models import CustomUser
 from django.contrib.auth import authenticate
 from drf_recaptcha.fields import ReCaptchaV2Field
 
 
-class CustomUserSerializer(DefaultRegisterUserSerializer):
-    password = serializers.CharField(write_only=True)
+class V2Serializer(Serializer):
+    recaptcha = ReCaptchaV2Field()
+
+
+class CustomUserRegisterSerializer(ModelSerializer, V2Serializer):
+    password = CharField(write_only=True)
+    password_confirm = CharField(write_only=True)
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password', 'password_confirm', 'recaptcha')
+
+    def create(self, validated_data):
+        validated_data.pop('recaptcha')
+        validated_data.pop('password_confirm')
+        return super().create(validated_data)
+
+
+class CustomUserLoginSerializer(ModelSerializer, V2Serializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'recaptcha')
         extra_kwargs = {
             'recaptcha': {'required': True},
         }
-
-    def get_fields(self):
-        fields = super().get_fields()
-        fields['recaptcha'] = ReCaptchaV2Field()
-        return fields
-
-    def create(self, validated_data):
-        password_confirm = validated_data.pop('password_confirm', None)
-        recaptcha = validated_data.pop('recaptcha', None)
-        user = CustomUser.objects.create_user(**validated_data)
-        return user
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Remove 'first_name' and 'last_name' fields from serializer
-        self.fields.pop('first_name', None)
-        self.fields.pop('last_name', None)
-        self.fields.pop('email_confirmed', None)
-
-
-class CustomLoginSerializer(DefaultLoginSerializer):
 
     def validate(self, data):
         email = data.get('email')
@@ -44,10 +38,10 @@ class CustomLoginSerializer(DefaultLoginSerializer):
             user = authenticate(request=self.context.get('request'), username=email, password=password)
 
             if not user:
-                raise serializers.ValidationError("Invalid login credentials. Please try again.")
+                raise ValidationError("Invalid login credentials. Please try again.")
 
             data['user'] = user
         else:
-            raise serializers.ValidationError("Both email and password are required for login.")
+            raise ValidationError("Both email and password are required for login.")
 
         return data
