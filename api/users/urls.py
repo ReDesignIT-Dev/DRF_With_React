25c685e3a6b14c52940bd30e87cb14513d81b5b9
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-11 21:19:03 +0200

    delet rest registration part

--- api/users/urls.py
+++ api/users/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='api-register'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
-    path('accounts/', include('rest_registration.api.urls'))
 ]
