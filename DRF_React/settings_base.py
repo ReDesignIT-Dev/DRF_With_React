fa8d63ccfc566f009fea307e72cf1cbb0a528152
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-12 20:20:29 +0200

    change default permission

--- DRF_React/settings_base.py
+++ DRF_React/settings_base.py
@@ -32,7 +32,7 @@ REGISTRATION_SALT = getenv('REGISTRATION_SALT')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
