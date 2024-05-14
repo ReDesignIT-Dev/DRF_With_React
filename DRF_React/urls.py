ad9dc675bec19d147764422b7af45c3ca1c450d7
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-14 20:21:32 +0200

    add knox urls

--- DRF_React/urls.py
+++ DRF_React/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.shop.urls')),
     path('', include('api.users.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
--- api/users/urls.py
+++ api/users/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include
-from .views import RegisterView, LoginView, LogoutView
+from django.urls import path
+from .views import RegisterView, LoginView, LogoutView, ForLoggedInOnlyView
+from knox import views as knox_views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='api-register'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='api-logoutall'),
+    path('secrets/', ForLoggedInOnlyView.as_view(), name='secret-content'),
 ]
