dace55f90e4a1aa2f219427806c464ae1e70b485
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-16 18:16:56 +0200

    customize logoutall to have always 200 in return

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, ForLoggedInOnlyView
+from .views import RegisterView, LoginView, LogoutView, LogoutAllView, ForLoggedInOnlyView
 from knox import views as knox_views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='api-register'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
-    path('logoutall/', knox_views.LogoutAllView.as_view(), name='api-logoutall'),
+    path('logoutall/', LogoutAllView.as_view(), name='api-logoutall'),
     path('secrets/', ForLoggedInOnlyView.as_view(), name='secret-content'),
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -4,14 +4,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CustomUserLoginSerializer, CustomUserRegisterSerializer
-from django.contrib.auth import logout
-# from rest_framework.authentication import TokenAuthentication
-# from rest_framework.authtoken.models import Token
-from django.contrib.auth import login
 from knox.views import LoginView as KnoxLoginView
 from knox.views import LogoutView as KnoxLogoutView
+from knox.views import LogoutAllView as KnoxLogoutAllView
 from knox.auth import TokenAuthentication
 from rest_framework.authentication import BasicAuthentication
+from django.contrib.auth.signals import user_logged_out
 
 
 class RegisterView(generics.CreateAPIView):
@@ -40,27 +38,40 @@ class RegisterView(generics.CreateAPIView):
 
 class LoginView(KnoxLoginView):
     authentication_classes = [BasicAuthentication]
-    serializer_class = CustomUserLoginSerializer  # serializer taken from knox in settings.py
+    serializer_class = CustomUserLoginSerializer
 
     def get_post_response_data(self, request, token, instance):
+        data = {
+            'expiry': self.format_expiry_datetime(instance.expiry),
+            'token': token
+        }
         serializer = self.serializer_class(data=request.data, context={"request": request})
-
         if serializer.is_valid(raise_exception=True):
             user = serializer.validated_data['user']
-            data = {'expiry': self.format_expiry_datetime(instance.expiry), 'token': token, 'user': user.username}
-            return data
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            data['user'] = user.username
+        return data
 
 
 class LogoutView(KnoxLogoutView):
-    def get_post_response(self, request):
-        return Response({"bye-bye": request.user.username}, status=200)
+
+    def post(self, request, format=None):
+        request._auth.delete()
+        user_logged_out.send(sender=request.user.__class__,
+                             request=request, user=request.user)
+        return Response(None, status=status.HTTP_200_OK)
+
+
+class LogoutAllView(KnoxLogoutAllView):
+
+    def post(self, request, format=None):
+        request.user.auth_token_set.all().delete()
+        user_logged_out.send(sender=request.user.__class__,
+                             request=request, user=request.user)
+        return Response(None, status=status.HTTP_200_OK)
 
 
 class ForLoggedInOnlyView(APIView):
     authentication_classes = (TokenAuthentication,)
-
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
