2b83e44e2a0268a8a0cab2b3cf4d0d2dc403c66b
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-20 18:33:31 +0200

    password recovery url

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, LogoutAllView, ForLoggedInOnlyView, UserActivationView
+from .views import RegisterView, LoginView, LogoutView, LogoutAllView, ForLoggedInOnlyView, UserActivationView, \
+    UserPasswordResetView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='api-register'),
     path('activate/<str:token>/', UserActivationView.as_view(), name='activate-user'),
+    path('password-reset/', UserPasswordResetView.as_view(), name='password-reset'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
-    path('logoutall/', LogoutAllView.as_view(), name='api-logoutall'),
+    path('logout-all/', LogoutAllView.as_view(), name='api-logout-all'),
     path('secrets/', ForLoggedInOnlyView.as_view(), name='secret-content'),
 ]
