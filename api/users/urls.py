64d6e34a4695418ddc2f34a2f85b50074aec19b0
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-05-24 23:26:01 +0200

    add password reset activation url

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import RegisterView, LoginView, LogoutView, LogoutAllView, ForLoggedInOnlyView, UserActivationView, \
-    UserPasswordResetView
+    UserPasswordResetView, UserPasswordResetActivationView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='api-register'),
     path('activate/<str:token>/', UserActivationView.as_view(), name='activate-user'),
     path('password-reset/', UserPasswordResetView.as_view(), name='password-reset'),
+    path('password-reset/<str:token>/', UserPasswordResetActivationView.as_view(),
+         name='password-reset-activation'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutView.as_view(), name='api-logout'),
     path('logout-all/', LogoutAllView.as_view(), name='api-logout-all'),
