a33c4b1e5b1e49f4ef82109a278876333dbd5bf6
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-01 16:57:11 +0200

    add setting new password logic serializer

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from django.template.loader import render_to_string
+from django.utils import timezone
 from rest_framework.fields import empty
 from rest_framework.serializers import Serializer, ModelSerializer, CharField, ValidationError, EmailField
 from .email_sender import EmailSender
@@ -57,35 +59,59 @@ class PasswordResetSerializer(Serializer):
                     raise ValidationError('User banned')
                 else:
                     raise ValidationError('User not activated yet')
-            elif self.user.activation_token is not None:
+            elif self.user.password_reset_token is not None:
                 raise ValidationError('Password recovery link already sent')
-            else:
-                self.user.generate_activation_token()
-                self.send_password_recovery_email(self.user)
         except ObjectDoesNotExist:
             raise ValidationError('User with this email does not exist')
         return email
 
-    def send_password_recovery_email(self, user):
+    def send_password_recovery_email(self):
         request = self.context.get('request')
         if not request:
             raise ValidationError('Request context is missing')
-        recovery_path = reverse('password-reset-activation', kwargs={'token': user.activation_token})
+        recovery_path = reverse('password-reset-activation', kwargs={'token': self.user.password_reset_token})
         recovery_link = request.build_absolute_uri(recovery_path)
         html_content = render_to_string('user_password_recovery_template.html', {
-            'username': user.username,
+            'username': self.user.username,
             'recovery_link': recovery_link,
         })
         with EmailSender() as email_sender:
             email_sender.send_email(
-                recipient=user.email,
+                recipient=self.user.email,
                 subject='Password reset',
                 message_content=html_content
             )
 
+    def save(self):
+        if not self.user:
+            raise ValidationError("User not found. Validate the token first.")
+        self.user.generate_password_reset_token()
 
-class PasswordResetActivationSerializer(Serializer):
+
+class TokenSerializer(Serializer):
     token = CharField()
+
+
+class PasswordResetActivationSerializer(TokenSerializer):
+    class Meta:
+        model = CustomUser
+
+    def __init__(self, instance=None, data=empty, **kwargs):
+        super().__init__(instance, data, **kwargs)
+        self.user = None
+
+    def validate_token(self, value):
+        try:
+            self.user = self.Meta.model.objects.get(password_reset_token=value)
+        except ObjectDoesNotExist:
+            raise ValidationError('Invalid password recovery token')
+        return value
+
+
+class PasswordSetNewPasswordSerializer(TokenSerializer):
+    password = CharField(max_length=128)
+    password_confirm = CharField(max_length=128)
+    TOKEN_EXPIRE_TIME_HOURS = 1
 
     class Meta:
         model = CustomUser
@@ -96,15 +122,34 @@ class PasswordResetActivationSerializer(Serializer):
 
     def validate_token(self, value):
         try:
-            self.user = self.Meta.model.objects.get(activation_token=value)
+            self.user = self.Meta.model.objects.get(password_reset_token=value)
         except ObjectDoesNotExist:
             raise ValidationError('Invalid password recovery token')
+
+        if self.user.password_reset_token_created_at:
+            token_age = timezone.now() - self.user.password_reset_token_created_at
+            if token_age > timedelta(hours=self.TOKEN_EXPIRE_TIME_HOURS):
+                self.user.password_reset_token = None
+                self.user.save()
+                raise ValidationError('Password recovery token has expired')
+
         return value
 
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_confirm = attrs.get('password_confirm')
 
-class PasswordSetNewPasswordSerializer(Serializer):
-    password = CharField(max_length=128)
-    password_repeat = CharField(max_length=128)
+        if password != password_confirm:
+            raise ValidationError("Passwords do not match")
+        validate_password(password)
+        return attrs
+
+    def save(self):
+        if not self.user:
+            raise ValidationError("User not found. Validate the token first.")
+        self.user.set_password(self.validated_data['password'])
+        self.user.password_reset_token = None
+        self.user.save()
 
 
 class CustomUserRegisterSerializer(ModelSerializer, V2Serializer):
