95628f4f33735c8f1025c189f42c9758b34e0ff6
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-05 20:58:26 +0200

    update naming

--- api/shop/urls.py
+++ api/shop/urls.py
@@ -5,11 +5,11 @@ from .views import ProductList, ProductCreate, ProductEditView, HomeView, Produc
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/', ProductList.as_view(), name='products'),
-    path('products/create', ProductCreate.as_view(), name='product_create'),
+    path('products/create/', ProductCreate.as_view(), name='product_create'),
     path('products/<slug:slug>/', ProductView.as_view(), name='product'),
-    path('products/<slug:slug>/edit', ProductEditView.as_view(), name='product_edit'),
+    path('products/<slug:slug>/edit/', ProductEditView.as_view(), name='product_edit'),
     path('categories/', CategoriesView.as_view(), name='categories'),
-    path('categories/create', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
     path('categories/<slug:slug>/', CategoryView.as_view(), name='category'),
-    path('categories/<slug:slug>/edit', CategoryEditView.as_view(), name='category_edit')
+    path('categories/<slug:slug>/edit/', CategoryEditView.as_view(), name='category_edit')
 ]
