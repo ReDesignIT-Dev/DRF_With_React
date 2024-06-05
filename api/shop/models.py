3f004bb5064427a276bf666711625d0ede20c442
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-05 21:00:54 +0200

    add mptt model for category and bring back the fields for product

--- api/shop/models.py
+++ api/shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel, TreeForeignKey
 from api.users.models import CustomUser
 from django.utils.text import slugify
 
@@ -25,17 +25,36 @@ class CommonFields(models.Model):
         abstract = True
 
 
-class Category(CommonFields):
+class Category(CommonFields, MPTTModel):
     description = models.TextField(blank=True, null=True)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.SET_NULL)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', default=1,
+                            on_delete=models.SET_DEFAULT)
 
     class MPTTMeta:
         order_insertion_by = ['level']
+
+    def delete(self, *args, **kwargs):
+        # Update products to point to the parent category
+        parent_category = self.parent
+        for product in Product.objects.filter(categories=self):
+            product.categories.remove(self)
+            if parent_category:
+                product.categories.add(parent_category)
+
+        # Update child categories to point to the parent category
+        self.children.update(parent=self.parent)
+
+        # Call the superclass delete method to perform the actual delete
+        super().delete(*args, **kwargs)
 
 
 class Product(CommonFields):
     categories = models.ManyToManyField(Category)
     image = models.ImageField(blank=True, default='shop_default_image.jpg', upload_to="products")
+    description = models.CharField(max_length=500, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_start = models.DateTimeField(null=True, blank=True)
+    sale_end = models.DateTimeField(null=True, blank=True)
 
 
 class ShoppingCart(CommonFields):
