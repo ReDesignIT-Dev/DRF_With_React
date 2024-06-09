6b5271033f93fb0440bffb97f20d29ff132f8cff
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-09 20:36:42 +0200

    add admin view to simplify changes in user settings

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # The fields to be used in displaying the User model.
+    list_display = ('email', 'username', 'email_confirmed', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'email_confirmed')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'email_confirmed',
+                                    'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (
+        'Additional info', {'fields': ('activation_token', 'password_reset_token', 'password_reset_token_created_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+# Register your model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
