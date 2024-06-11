803b0de9bdf7074faaadccd2dd2b61831e52e27f
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-11 17:38:41 +0200

    add static urls

--- DRF_React/urls.py
+++ DRF_React/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf.urls.static import serve
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.shop.urls')),
     path('', include('api.users.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico', permanent=True))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
