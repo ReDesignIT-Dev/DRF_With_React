7a2da79b55e997b19868a6113d19d4ae6977eef8
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-17 17:17:54 +0200

    test page to check working with cookie token

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,4 +1,27 @@
+import { useEffect } from 'react';
+import { getDataUsingUserToken } from "services/apiRequests";
+import Cookies from 'js-cookie';
+
 export default function About() {
+
+  async function getSecrets() {
+    try {
+      const token = Cookies.get('token');
+      await getDataUsingUserToken("secrets/", token).then((response) => {
+        console.log("response: ", response);
+        if (response.status === 200) {
+          console.log("OK");
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getSecrets();
+  }, []);
+
   return (
     <div>
       <h1> About this website </h1>
