77b6cbbcbaaf817b94041e9e25926a72503a577b
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-18 17:40:06 +0200

    small cleanup

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,8 @@
-import { useState, useEffect } from 'react';
 export default function Home() {
-  const [message, setMessage] = useState('');
 
   return (
     <div>
       <h1> Main page </h1>
- 
-      <p>{message}</p>
 
       <h1>Welcome to Our Shop</h1>
 
