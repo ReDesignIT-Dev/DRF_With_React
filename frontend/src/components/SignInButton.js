c6d86938a836b6078eb3b6e96bfb21bdd2564bd5
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-18 17:39:53 +0200

    adapt to change after login to  Log Out name

--- frontend/src/components/SignInButton.js
+++ frontend/src/components/SignInButton.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SignInButton.css";
 import "react-bootstrap";
+import { getToken, removeToken } from "utils/cookies";
+import { logoutUser } from "services/apiRequests";
 
 export default function SignInButton({ handleLoginClick }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const handleClick = () => {
-    handleLoginClick();
+    if (isLoggedIn) {
+      const token = getToken();
+      try {
+        logoutUser(token);
+      } catch (error) {
+        console.log(error.message);
+      }
+      removeToken();
+      setIsLoggedIn(false);
+    } else {
+      handleLoginClick();
+    }
   };
+
   return (
-      <div onClick={handleClick} className="loginicon">
-          Sign In
-      </div>
+    <div onClick={handleClick} className="loginicon">
+      {isLoggedIn ? "Log Out" : "Sign In"}
+    </div>
   );
 }
