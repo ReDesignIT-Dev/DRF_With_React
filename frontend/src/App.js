69a949748ea086367b3e3ec8f65adaf228f1100d
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-19 19:17:19 +0200

    use the config urls in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,9 @@ import Contact from "pages/Contact";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import NotFound from "pages/NotFound";
-import PasswordReset from "pages/PasswordReset";
+import PasswordReset from "pages/PasswordReset.js";
+import UserActivation from "pages/UserActivation.js";
+import { PASSWORD_RESET_URL, ACTIVATE_USER_URL } from 'config';
 
 function App() {
   return (
@@ -16,8 +18,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/password-reset/:token" element={<PasswordReset />} />
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path={PASSWORD_RESET_URL} element={<PasswordReset />} />
+        <Route path={ACTIVATE_USER_URL} element={<UserActivation />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
