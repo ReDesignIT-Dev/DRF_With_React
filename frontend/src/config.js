b719f7122f04329cdac934b2f7f616e52c10781c
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-19 19:16:39 +0200

    keep all urls in one place to work well with backend calls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:8000';
-
-export default BASE_URL;
+export const BASE_URL = "http://localhost:8000";
+export const PASSWORD_RESET_URL = "/password-reset/:token";
+export const ACTIVATE_USER_URL = "/activate/:token";
