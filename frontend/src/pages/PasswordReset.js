e59bc13a62992bd184f587a11dae6f89ea327318
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-19 19:18:13 +0200

    add passwordreset page to work with newly created field for new password

--- frontend/src/pages/PasswordReset.js
+++ frontend/src/pages/PasswordReset.js
@@ -1,55 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import NewPasswordField from 'components/Fields/NewPasswordField';
+// import {
+//   isEmpty,
+//   isPasswordValid,
+//   isTheSamePassword,
+//   isLengthValid,
+//   isUppercaseValid,
+//   isLowercaseValid,
+//   isDigitValid,
+//   isSpecialCharValid,
+// } from "utils/validation";
+
+//TODO FIELD WITH VALIDATION
 
 const PasswordReset = () => {
   const { token } = useParams();
 
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (password !== passwordConfirm) {
-      setErrorMessage('Passwords do not match');
-      return;
- 
-    }
-    setPassword('');
-    setPasswordConfirm('');
-    setErrorMessage('');
-}
 
 return (
-    <div className="container">
-      <h1>Password Reset</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="passwordConfirm" className="form-label">Confirm Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="passwordConfirm"
-            value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
-            required
-          />
-        </div>
-        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-        <button type="submit" className="btn btn-primary">Reset Password</button>
-      </form>
+    <div className="container d-flex flex-column align-items-center">
+      {token}
+      <div className='' style={{ maxWidth: '200px' }}>
+     <NewPasswordField />
+     </div>
     </div>
   );
 };
