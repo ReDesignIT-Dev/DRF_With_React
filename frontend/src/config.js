0d71ef4b2203993ba099b1d86935935b26207820
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-20 19:34:01 +0200

    add endpoint for query if token is ok for password reset

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 export const BASE_URL = "http://localhost:8000";
+export const PASSWORD_RESET_API_URL = "/password-reset/";
 export const PASSWORD_RESET_URL = "/password-reset/:token";
 export const ACTIVATE_USER_URL = "/activate/:token";
