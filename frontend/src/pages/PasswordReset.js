9dc6eafdf06bd565118abd3cace5e8f665a20f46
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-20 19:36:54 +0200

    add password reset page with loading while getting the token validation. If token is valid show fields for new password

--- frontend/src/pages/PasswordReset.js
+++ frontend/src/pages/PasswordReset.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import NewPasswordField from 'components/Fields/NewPasswordField';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NewPasswordField from "components/Fields/NewPasswordField";
+import { PASSWORD_RESET_API_URL } from "config";
+import { getDataUsingUserToken } from "services/apiRequests";
+import ReactLoading from "react-loading";
+import "./PasswordReset.scss";
 // import {
 //   isEmpty,
 //   isPasswordValid,
@@ -16,14 +20,53 @@ import NewPasswordField from 'components/Fields/NewPasswordField';
 
 const PasswordReset = () => {
   const { token } = useParams();
+  const [isValidToken, setIsValidToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
+  useEffect(() => {
+    const checkTokenValidity = async () => {
+      try {
+        const response = await getDataUsingUserToken(`${PASSWORD_RESET_API_URL}${token}`, token);
+        if (response.status === 200) {
+          setIsValidToken(true);
+        }
+      } catch (error) {
+        if (error.message.includes("Network Error")) {
+          setErrorMessage("Network Error: Please check your internet connection.");
+        } else if (error.message.includes("API Error")) {
+          setErrorMessage("Invalid token.");
+        } else {
+          setErrorMessage("An unknown error occurred.");
+        }
+        setIsValidToken(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkTokenValidity();
+  }, [token]);
 
-return (
-    <div className="container d-flex flex-column align-items-center">
-      {token}
-      <div className='' style={{ maxWidth: '200px' }}>
-     <NewPasswordField />
-     </div>
+  if (loading) {
+    return (
+      <div className='loading-div'>
+        <div className='loading-component mb-5 mt-5'>
+          <ReactLoading type={"spinningBubbles"} color={"green"} height={"100%"} width={"100%"} />
+        </div>
+        <div className='fs-3'>Please wait</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='container d-flex flex-column align-items-center'>
+      {isValidToken ? (
+        <div className='' style={{ maxWidth: "200px" }}>
+          <NewPasswordField />
+        </div>
+      ) : (
+        <div>{errorMessage}</div> // Show an error message if the token is invalid
+      )}
     </div>
   );
 };
