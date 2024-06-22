5943388e92cc52f59eda8be3308c783f94981e2e
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-22 22:51:20 +0200

    ignore prop type

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,31 +1,26 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2020,
-      sourceType: 'module',
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": ["error", { varsIgnorePattern: "^_" }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    plugins: [
-      'react',
-    ],
-    rules: {
-      'no-unused-vars': ['error', { 'varsIgnorePattern': '^_' }],
-      'react/react-in-jsx-scope': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
-  
+  },
+};
