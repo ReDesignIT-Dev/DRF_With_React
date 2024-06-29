85388ce32a36587eed24ed4ada3ae0e356561e9c
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-29 12:33:31 +0200

    add simplest activation page and logic - TODO styling later

--- frontend/src/pages/UserActivation.js
+++ frontend/src/pages/UserActivation.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import { activateUser } from 'services/apiRequests';
 
 const UserActivation = () => {
   const { token } = useParams();
+  const [message, setMessage] = useState(null);
 
-// TODO LOGIC
+  useEffect(() => {
+    const activate = async () => {
+      try {
+        const response = await activateUser(token);
+        const returnMessage = response.data.message;
+        if (response.status === 200) {
+          setMessage(returnMessage);
+        } else {
+          setMessage("Failed to activate.");
+        }
+      } catch (error) {
+        setMessage("An error occurred. Please try again.");
+      }
+    };
+
+    activate();
+  }, [token]);
+
+
 return (
     <div className="container">
-     {token}
+     {message}
     </div>
   );
 };
