b7516c363af00584472183247f563aead7aaa7e6
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-06-30 19:33:09 +0200

    add success + error messages after the post and add loading element while waiting for response

--- frontend/src/pages/PasswordRecovery.js
+++ frontend/src/pages/PasswordRecovery.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import RecaptchaField from "components/Fields/RecaptchaField";
 import EmailField from "components/Fields/EmailField";
 import { postPasswordRecovery } from "services/apiRequests";
+import Loading from "components/Loading";
 
 const PasswordRecovery = () => {
   const [isValid, setIsValid] = useState(false);
@@ -9,56 +10,75 @@ const PasswordRecovery = () => {
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [reCaptchaToken, setReCaptchaToken] = useState("");
   const [isValidReCaptchaToken, setIsValidRecaptchaToken] = useState(false);
+  const [postSuccess, setPostSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const valid = (isEmailValid && isValidReCaptchaToken);
-    console.log(isValid);
+    const valid = isEmailValid && isValidReCaptchaToken;
     setIsValid(valid);
   }, [email, isEmailValid, isValidReCaptchaToken]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (isValid) {
+      setLoading(true);
+      setErrorMessage("");
       try {
-        const response = await postPasswordRecovery(
-          email,
-          reCaptchaToken
-        );
+        const response = await postPasswordRecovery(email, reCaptchaToken);
+        const returnMessage = response.data.message;
         if (response.status === 200) {
-          const returnMessage = response.data.message;
+          setPostSuccess(true);
+          setSuccessMessage(returnMessage);
+          setErrorMessage("");
           console.log(returnMessage);
         } else {
-          const errorData = response.data;
-          console.log(errorData);
+          setErrorMessage(returnMessage);
         }
       } catch (error) {
-        console.log(error.message);
+          setErrorMessage(error.message);
+          console.log(error.message);
+      } finally {
+        setLoading(false);
       }
     } else {
-      console.log("NOT VALID");
+      setErrorMessage("FORM IS NOT VALID");
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='d-flex flex-column justify-content-center align-items-center'
-    >
-      <label className="text-black">{"Enter your user account's verified email address and we will send you a password reset link."}</label>
-      <EmailField
-        customClasses='my-2 text-center'
-        onChange={setEmail}
-        onValidate={setIsEmailValid}
-      />
-      <RecaptchaField onValidate={setIsValidRecaptchaToken} setReturnToken={setReCaptchaToken} />
-      <button
-        type='submit'
-        className='btn btn-primary mt-3'
-        disabled={!isValid}
-      >
-        Submit
-      </button>
-    </form>
+    <div>
+      {loading ? (
+        <Loading />
+      ) : postSuccess ? (
+        <label className="alert alert-success">{ successMessage }</label>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className='d-flex flex-column justify-content-center align-items-center'
+        >
+          <label className='text-black'>
+            {
+              "Enter your user account's verified email address and we will send you a password reset link."
+            }
+          </label>
+          <EmailField
+            customClasses='my-2 text-center'
+            onChange={setEmail}
+            onValidate={setIsEmailValid}
+          />
+          <RecaptchaField
+            onValidate={setIsValidRecaptchaToken}
+            setReturnToken={setReCaptchaToken}
+          />
+          <button type='submit' className='btn btn-primary mt-3' disabled={!isValid}>
+            Submit
+          </button>
+          {errorMessage && <label className="alert alert-warning">{errorMessage}</label>}
+        </form>
+      )}
+    </div>
   );
 };
 
