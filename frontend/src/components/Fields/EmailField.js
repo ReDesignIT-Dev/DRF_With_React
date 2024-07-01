99beac7fe1313c7fdf5ad52a4a08b146a411824b
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-01 20:36:55 +0200

    super simplify email field and add value to keep it in form in case of error while posting

--- frontend/src/components/Fields/EmailField.js
+++ frontend/src/components/Fields/EmailField.js
@@ -1,27 +1,26 @@
-import { useState, useEffect } from "react";
 import { isEmailValid } from "utils/validation";
 import "./EmailField.scss";
 
-export default function EmailField({ disabled, customClasses, onChange, onValidate }) {
-  const [email, setEmail] = useState("");
-
-  useEffect(() => {
-    const isValid = isEmailValid(email);
+export default function EmailField({ value, disabled, customClasses, onChange, onValidate }) {
+  const validateField = (valueToValidate) => {
+    const isValid = isEmailValid(valueToValidate);
     onValidate(isValid);
-  }, [email]);
+  };
 
   const handleChange = (e) => {
     const value = e.target.value;
-    setEmail(value);
     onChange(value);
+    validateField(value);
   };
 
   return (
     <input
+      value={value}
       className={customClasses}
-      type="email"
-      id="emailField"
-      placeholder="email"
+      type='email'
+      id='emailField'
+      placeholder='email'
+      autoComplete='email'
       onChange={handleChange}
       disabled={disabled}
     />
