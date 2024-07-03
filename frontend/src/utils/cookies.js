fb478258c8676bc55bc951639d3266deb27893b2
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-03 18:07:12 +0200

    move date to request - cookie has to receive Date format

--- frontend/src/utils/cookies.js
+++ frontend/src/utils/cookies.js
@@ -9,6 +9,6 @@ export const removeToken = () => {
 };
 
 export const setToken = (token, expire) => {
-  const expiryDate = new Date(expire);
-  Cookies.set("token", token, { secure: true, sameSite: "Strict", expires: expiryDate });
+  
+  Cookies.set("token", token, { secure: true, sameSite: "Strict", expires: expire });
 };
