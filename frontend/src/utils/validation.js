ebc069c8cc930abf580c1b056c6a2a4442493438
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-05 14:00:08 +0200

    add import

--- frontend/src/utils/validation.js
+++ frontend/src/utils/validation.js
@@ -1,3 +1,5 @@
+import { getToken } from "./cookies";
+
 export function isEmailValid(emailToTest) {
   const emailRegex = 
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
