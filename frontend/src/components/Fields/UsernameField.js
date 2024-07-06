717cdebca2badd2001fa8bc19e02ae594a72e859
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-06 13:25:54 +0200

    add error label to notify user that there is a requirement 3-30 chars

--- frontend/src/components/Fields/UsernameField.js
+++ frontend/src/components/Fields/UsernameField.js
@@ -2,9 +2,15 @@ import { useState, useEffect } from "react";
 
 export default function UsernameField({ disabled, customClasses, onChange, onValidate }) {
   const [username, setUsername] = useState("");
+  const [usernameFieldError, setUsernameFieldError] = useState("");
 
   useEffect(() => {
     const isValid = isUsernameValid(username);
+    if (isValid) {
+      setUsernameFieldError("");
+    } else {
+      setUsernameFieldError("Require 3-30 chars");
+    }
     onValidate(isValid);
   }, [username]);
 
@@ -15,17 +21,20 @@ export default function UsernameField({ disabled, customClasses, onChange, onVal
   };
 
   const isUsernameValid = (value) => {
-    return (value.length > 2 && value.length < 20)
-  }
+    return value.length >= 3 && value.length <= 30;
+  };
 
   return (
-    <input
-      className={customClasses}
-      type="text"
-      id="usernameField"
-      placeholder="username"
-      onChange={handleChange}
-      disabled={disabled}
-    />
+    <div className="d-flex flex-column">
+      <input
+        className={customClasses}
+        type='text'
+        id='usernameField'
+        placeholder='username'
+        onChange={handleChange}
+        disabled={disabled}
+      />
+      <label className='text-danger'>{usernameFieldError}</label>
+    </div>
   );
 }
