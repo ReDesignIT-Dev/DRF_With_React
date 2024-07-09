fd58bd7bde1718ff52edd184f931bface6a28f4c
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-09 20:12:12 +0200

    working register fields with multiple errors from username and email

--- frontend/src/components/RegisterFormComponent.js
+++ frontend/src/components/RegisterFormComponent.js
@@ -6,26 +6,75 @@ import { useEffect, useState } from "react";
 import Loading from "components/Loading";
 import { registerUser } from "services/apiRequests";
 import "./RegisterFormComponent.css"; // Import the CSS file
+import { GeneralApiError, MultipleFieldErrors } from "services/CustomErrors";
 
 const RegisterFormComponent = ({ isLoggedIn, onRegisterSuccess }) => {
-
   const [isValid, setIsValid] = useState(false);
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState(localStorage.getItem("username") || "");
+  const [usernameFieldError, setUsernameFieldError] = useState(localStorage.getItem("usernameFieldError") || "");
   const [isUsernameValid, setIsUsernameValid] = useState(false);
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState(localStorage.getItem("email") || "");
+  const [emailFieldError, setEmailFieldError] = useState(localStorage.getItem("emailFieldError") || "");
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [reCaptchaToken, setReCaptchaToken] = useState("");
   const [isValidReCaptchaToken, setIsValidRecaptchaToken] = useState(false);
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [isPasswordWithPasswordConfirmValid, setIsPasswordWithPasswordConfirmValid] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [isPasswordWithPasswordConfirmValid, setIsPasswordWithPasswordConfirmValid] =
+    useState(false);
+  const [errorMessage, setErrorMessage] = useState(localStorage.getItem("detailError") || "");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const valid = isEmailValid && isValidReCaptchaToken && isPasswordWithPasswordConfirmValid && isUsernameValid;
+    localStorage.removeItem("usernameFieldError");
+    localStorage.setItem("username", username);
+    validateUsername();
+  }, [username]);
+
+  useEffect(() => {
+    localStorage.removeItem("emailFieldError");
+    localStorage.setItem("email", email);
+    validateEmail();
+  }, [email]);
+
+  const validateEmail = () => {
+    const emailFieldErrorFromPost = localStorage.getItem("emailFieldError");
+    if (emailFieldErrorFromPost){
+      setEmailFieldError(emailFieldErrorFromPost);
+    }
+    else if (!isEmailValid) {
+      setEmailFieldError("Invalid email");
+    }
+    else {
+      setEmailFieldError("");
+    }
+  };
+
+  const validateUsername = () => {
+    const usernameFieldErrorFromPost = localStorage.getItem("usernameFieldError");
+    if (usernameFieldErrorFromPost){
+      setUsernameFieldError(usernameFieldErrorFromPost);
+    }
+    else if (!isUsernameValid) {
+      setUsernameFieldError("Username must be between 3 and 30 chars");
+    }
+    else {
+      setUsernameFieldError("");
+    }
+  };
+
+  useEffect(() => {
+    const valid =
+      isEmailValid &&
+      isValidReCaptchaToken &&
+      isPasswordWithPasswordConfirmValid &&
+      isUsernameValid;
     setIsValid(valid);
+    validateUsername();
+    validateEmail();
+
   }, [isEmailValid, isValidReCaptchaToken, isPasswordWithPasswordConfirmValid, isUsernameValid]);
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -33,9 +82,16 @@ const RegisterFormComponent = ({ isLoggedIn, onRegisterSuccess }) => {
       setLoading(true);
       setErrorMessage("");
       try {
-        const response = await registerUser(username, email, password, passwordConfirm, reCaptchaToken);
+        const response = await registerUser(
+          username,
+          email,
+          password,
+          passwordConfirm,
+          reCaptchaToken
+        );
         const returnMessage = response.data.message;
         if (response.status === 200) {
+          localStorage.clear();
           onRegisterSuccess();
           setErrorMessage("");
         } else {
@@ -43,8 +99,31 @@ const RegisterFormComponent = ({ isLoggedIn, onRegisterSuccess }) => {
           console.log(returnMessage);
         }
       } catch (error) {
-        setErrorMessage(error.message);
-        console.log(error.message);
+        if (error instanceof MultipleFieldErrors) {
+          error.errors.forEach((err) => {
+            if (err.field === "username") {
+              setUsernameFieldError(err.message);
+              localStorage.setItem("usernameFieldError", err.message);
+              console.log(`Username Error: ${err.message}`);
+            }
+            if (err.field === "email") {
+              setEmailFieldError(err.message);
+              localStorage.setItem("emailFieldError", err.message);
+              console.log(`Email Error: ${err.message}`);
+            }
+            if (err.field === "detail") {
+              setErrorMessage(err.message);
+              localStorage.setItem("detailError", err.message);
+              console.log(`Detail Error: ${err.message}`);
+            }
+          });
+        } else if (error instanceof GeneralApiError) {
+          setErrorMessage(error.message);
+          console.log(error.message);
+        } else {
+          setErrorMessage("An unexpected error occurred.");
+          console.log("An unexpected error occurred:", error);
+        }
       } finally {
         setLoading(false);
       }
@@ -58,34 +137,37 @@ const RegisterFormComponent = ({ isLoggedIn, onRegisterSuccess }) => {
       {loading ? (
         <Loading />
       ) : isLoggedIn ? (
-        <label className="alert alert-success">{"You are logged in, you cannot register. Log Out to register"}</label>
+        <label className='alert alert-success'>
+          {"You are logged in, you cannot register. Log Out to register"}
+        </label>
       ) : (
         <form
           onSubmit={handleSubmit}
-          className="register-form-custom d-flex flex-column justify-content-center align-items-center mx-auto"
+          className='register-form-custom d-flex flex-column justify-content-center align-items-center mx-auto'
         >
-            <div className="input-group-register">
-            <label className="input-label-register">Username</label>
+          <div className='input-group-register'>
             <UsernameField
               value={username}
-              customClasses="text-center mx-auto"
+              customClasses='text-center w-100'
               onChange={setUsername}
               onValidate={setIsUsernameValid}
             />
+            {usernameFieldError && (
+              <label className='text-danger'>{usernameFieldError}</label>
+            )}
           </div>
-          <div className="input-group-register">
-            <label className="input-label-register">Email</label>
+          <div className='input-group-register'>
             <EmailField
               value={email}
-              customClasses="text-center mx-auto"
+              customClasses='text-center w-100'
               onChange={setEmail}
               onValidate={setIsEmailValid}
             />
+            {emailFieldError && <label className='text-danger'>{emailFieldError}</label>}
           </div>
-          <div className="input-group-register">
-            <label className="input-label-register">Password</label>
+          <div className='input-group-register'>
             <NewPasswordWithPasswordRepeatField
-            customClasses="mx-auto"
+              customClassesForNewPassword={"w-100"}
               passwordValue={password}
               passwordRepeatValue={passwordConfirm}
               onChangePassword={setPassword}
@@ -97,10 +179,10 @@ const RegisterFormComponent = ({ isLoggedIn, onRegisterSuccess }) => {
             onValidate={setIsValidRecaptchaToken}
             setReturnToken={setReCaptchaToken}
           />
-          <button type="submit" className="btn btn-primary mt-3" disabled={!isValid}>
+          <button type='submit' className='btn btn-primary mt-3' disabled={!isValid}>
             Submit
           </button>
-          {errorMessage && <label className="alert alert-warning">{errorMessage}</label>}
+          {errorMessage && <label className='alert alert-warning'>{errorMessage}</label>}
         </form>
       )}
     </div>
