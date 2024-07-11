20c8b1d343dce13fad77f8a99799829d61c97016
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-11 20:43:48 +0200

    redirect to login after successful activation

--- frontend/src/pages/UserActivation.js
+++ frontend/src/pages/UserActivation.js
@@ -1,24 +1,37 @@
+import { FRONTEND_LOGIN_URL } from 'config';
 import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { activateUser } from 'services/apiRequests';
+import Loading from "components/Loading";
 
 const UserActivation = () => {
   const { token } = useParams();
-  const [message, setMessage] = useState(null);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
+    
     const activate = async () => {
+      if (token && !success) {
+        setLoading(true);
       try {
         const response = await activateUser(token);
-        const returnMessage = response.data.message;
         if (response.status === 200) {
-          setMessage(returnMessage);
+          setSuccess(true);
+          
+          navigate(`../../${FRONTEND_LOGIN_URL}`, {replace: true});
         } else {
           setMessage("Failed to activate.");
         }
       } catch (error) {
         setMessage("An error occurred. Please try again.");
       }
+      finally {
+        setLoading(false);
+      }
+    }
     };
 
     activate();
@@ -26,8 +39,10 @@ const UserActivation = () => {
 
 
 return (
-    <div className="container">
-     {message}
+    <div className="d-flex justify-content-center align-items-center">
+     {loading ? (
+        <Loading />
+      ) : (<h2>{message}</h2>)}
     </div>
   );
 };
