fddef5bb17de7980b29f18c331c28e6529600e5d
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-13 13:12:38 +0200

    Add redirection buttons in login and register

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,19 @@
 import LoginFormComponent from "components/LoginFormComponent";
+import { useNavigate } from "react-router-dom";
+import { FRONTEND_REGISTER_URL } from "config";
 
 const Login = ({ isLoggedIn, onLoginSuccess }) => {
- 
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className='d-flex align-items-center justify-content-center flex-column'>
+      <button
+        type='button'
+        className='btn btn-info mt-2'
+        onClick={() => {
+          navigate(FRONTEND_REGISTER_URL, { replace: true });
+        }}
+      >{`Don't have an account? Click here to register`}</button>
       <LoginFormComponent isLoggedIn={isLoggedIn} onLoginSuccess={onLoginSuccess} />
     </div>
   );
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,13 +1,23 @@
 import RegisterFormComponent from "components/RegisterFormComponent";
+import { FRONTEND_LOGIN_URL } from "config";
+import { useNavigate } from "react-router-dom";
 
-const Register = ({isLoggedIn}) => {
+const Register = ({ isLoggedIn }) => {
+    const navigate = useNavigate();
 
-    
-return (
-    <div>
-        <RegisterFormComponent isLoggedIn={isLoggedIn}/>
+  return (
+    <div className='d-flex align-items-center justify-content-center flex-column'>
+      <button
+        type='button'
+        className='btn btn-info mt-2'
+        onClick={() => {
+          navigate(FRONTEND_LOGIN_URL, { replace: true });
+        }}
+      >{`Already have an account? Click here to Login`}</button>
+
+      <RegisterFormComponent isLoggedIn={isLoggedIn} />
     </div>
-)
-}
+  );
+};
 
 export default Register;
