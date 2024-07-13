2a3c47c51f0d1d09145982b0f21990aef3690c7d
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-13 13:08:20 +0200

    add leading slash to have absolute paths using useNavigate instead of relative path

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,14 @@
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "http://localhost:8000";
 
-export const FRONTEND_LOGIN_URL = "login/";
-export const FRONTEND_REGISTER_URL = "register/";
-export const FRONTEND_ACTIVATE_USER_URL = "activate/:token/";
-export const FRONTEND_PASSWORD_RECOVERY_URL = "password-recovery";
-export const FRONTEND_PASSWORD_RESET_URL = "password-reset/:token/";
+export const FRONTEND_LOGIN_URL = "/login";
+export const FRONTEND_REGISTER_URL = "/register";
+export const FRONTEND_ACTIVATE_USER_URL = "/activate/:token/";
+export const FRONTEND_PASSWORD_RECOVERY_URL = "/password-recovery";
+export const FRONTEND_PASSWORD_RESET_URL = "/password-reset/:token/";
 
 // API urls should match the django urls
-export const API_LOGOUT_USER_URL = "logout/";
-export const API_LOGIN_USER_URL = "login/";
-export const API_ACTIVATE_USER_URL = "activate/";
-export const API_REGISTER_USER_URL = "register/";
-export const API_PASSWORD_RESET_URL = "password-reset/";
+export const API_LOGOUT_USER_URL = "/logout";
+export const API_LOGIN_USER_URL = "/login";
+export const API_ACTIVATE_USER_URL = "/activate";
+export const API_REGISTER_USER_URL = "/register";
+export const API_PASSWORD_RESET_URL = "/password-reset";
