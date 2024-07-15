9a4fa85d0d4fcea17060ca0f840361b0de475194
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-15 17:33:39 +0200

    get rid of the ending slashes

--- api/shop/urls.py
+++ api/shop/urls.py
@@ -4,14 +4,14 @@ from .views import ProductList, ProductCreate, ProductEditView, HomeView, Produc
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('products/', ProductList.as_view(), name='products'),
-    path('product/create/', ProductCreate.as_view(), name='product_create'),
+    path('products', ProductList.as_view(), name='products'),
+    path('product/create', ProductCreate.as_view(), name='product_create'),
     path('product/<slug:slug>', ProductView.as_view(), name='product'),
-    path('product/<slug:slug>/edit/', ProductEditView.as_view(), name='product_edit'),
-    path('categories/', CategoriesView.as_view(), name='categories'),
-    path('category/create/', CategoryCreateView.as_view(), name='category_create'),
-    path('category/<slug:slug>/', CategoryView.as_view(), name='category'),
-    path('category/<slug:slug>/edit/', CategoryEditView.as_view(), name='category_edit'),
+    path('product/<slug:slug>/edit', ProductEditView.as_view(), name='product_edit'),
+    path('categories', CategoriesView.as_view(), name='categories'),
+    path('category/create', CategoryCreateView.as_view(), name='category_create'),
+    path('category/<slug:slug>', CategoryView.as_view(), name='category'),
+    path('category/<slug:slug>/edit', CategoryEditView.as_view(), name='category_edit'),
     path('shopping-admin-panel', ShopAdminPanel.as_view(), name='shop_admin_panel'),
-    path('shopping-admin-panel/products/', ShopAdminPanelProducts.as_view(), name='shop_admin_panel_products'),
+    path('shopping-admin-panel/products', ShopAdminPanelProducts.as_view(), name='shop_admin_panel_products'),
 ]
