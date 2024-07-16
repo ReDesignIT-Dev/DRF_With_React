d6b5f0bad2cfff202bdb4c56e5dd683fd3b401e2
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-16 20:40:46 +0200

    add shop based endpoints

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,15 @@
 export const BASE_URL = "http://localhost:8000";
 
+// user login/register based
 export const FRONTEND_LOGIN_URL = "/login";
 export const FRONTEND_REGISTER_URL = "/register";
-export const FRONTEND_ACTIVATE_USER_URL = "/activate/:token/";
+export const FRONTEND_ACTIVATE_USER_URL = "/activate/:token";
 export const FRONTEND_PASSWORD_RECOVERY_URL = "/password-recovery";
-export const FRONTEND_PASSWORD_RESET_URL = "/password-reset/:token/";
+export const FRONTEND_PASSWORD_RESET_URL = "/password-reset/:token";
+
+// shop based
+export const FRONTEND_PRODUCT_URL = "/product/:kwargs";
+export const FRONTEND_CATEGORY_URL = "/category/:kwargs";
 
 // API urls should match the django urls
 export const API_LOGOUT_USER_URL = "/logout";
@@ -12,3 +17,6 @@ export const API_LOGIN_USER_URL = "/login";
 export const API_ACTIVATE_USER_URL = "/activate";
 export const API_REGISTER_USER_URL = "/register";
 export const API_PASSWORD_RESET_URL = "/password-reset";
+
+// API shop based
+export const API_PRODUCT_URL = "product/:productId";
