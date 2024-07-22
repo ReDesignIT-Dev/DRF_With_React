8eb1b79477877b29e81bb63da215659c85d636e5
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-22 18:59:14 +0200

    create first api calls for shop -> categories

---
+++ frontend/src/services/apiRequestsShop.js
@@ -0,0 +1,33 @@
+import apiClient from "services/axiosConfig";
+import {
+  API_CATEGORY_URL,
+  API_ALL_CATEGORIES
+} from "config";
+import { apiErrorHandler } from "./apiErrorHandler";
+
+// TODO optimize later to query one by one
+export async function getAllProductsInCategory(category) {
+    try {
+      const response = await apiClient.get(`${API_CATEGORY_URL}/${category}`, {
+        headers: {
+          ...apiClient.defaults.headers,
+        },
+      });
+      return response;
+    } catch (error) {
+      apiErrorHandler(error);
+    }
+  }
+
+  export async function getAllCategoryNamesAndSlugs() {
+    try {
+      const response = await apiClient.get(API_ALL_CATEGORIES, {
+        headers: {
+          ...apiClient.defaults.headers,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      apiErrorHandler(error);
+    }
+  }
