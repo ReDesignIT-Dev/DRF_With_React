fe66046dc3333aec157cfc77959c48590edbe681
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-27 14:35:28 +0200

    add mdb styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import App from "App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
