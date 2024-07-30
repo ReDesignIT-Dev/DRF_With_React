469b79d7092d2736f0262bf9425aa0737fe1b525
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-07-30 20:33:53 +0200

    upgrade category listing of products with style and top bar to have category history

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,14 +1,16 @@
 import ProductList from "components/ProductList";
 import CategoryTree from "components/CategoryTree";
+import CategoryTopBar from "components/CategoryTopBar";
+import "./Category.css";
 
 export default function Category() {
-
-
   return (
-    <div className="category-view-container d-flex flex-row">
-      < CategoryTree />
-      < ProductList /> 
-
+    <div className='category-view-container d-flex flex-column mx-auto gap-3'>
+      <CategoryTopBar className="bg-secondary mt-3"/>
+      <div className='d-flex flex-row gap-3'>
+        <CategoryTree className="bg-secondary category-tree-basis"/>
+        <ProductList className="bg-secondary product-list-basis"/>
+      </div>
     </div>
   );
 }
