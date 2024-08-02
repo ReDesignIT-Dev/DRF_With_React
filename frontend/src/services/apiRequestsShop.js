966c39dece3e47c6451d68e162351fbf192b29d5
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-02 19:00:44 +0200

    request to get children of a category with parent - name + slug

--- frontend/src/services/apiRequestsShop.js
+++ frontend/src/services/apiRequestsShop.js
@@ -1,7 +1,7 @@
 import apiClient from "services/axiosConfig";
 import {
   API_CATEGORY_URL,
-  API_ALL_CATEGORIES
+  API_ALL_CATEGORIES,
 } from "config";
 import { apiErrorHandler } from "./apiErrorHandler";
 
@@ -22,6 +22,19 @@ export async function getAllProductsInCategory(categorySlug) {
   export async function getAllCategoryNamesAndSlugs() {
     try {
       const response = await apiClient.get(API_ALL_CATEGORIES, {
+        headers: {
+          ...apiClient.defaults.headers,
+        },
+      });
+      return response;
+    } catch (error) {
+      apiErrorHandler(error);
+    }
+  }
+
+  export async function getAllChildrenOfCategory(categorySlug) {
+    try {
+      const response = await apiClient.get(`${API_CATEGORY_URL}/${categorySlug}/children`, {
         headers: {
           ...apiClient.defaults.headers,
         },
