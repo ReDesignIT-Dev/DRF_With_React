0b9b8cb0ec510729ea2e758937312e1ea5f4d7ce
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-02 19:01:07 +0200

    implement simple category tree

--- frontend/src/components/CategoryTree.js
+++ frontend/src/components/CategoryTree.js
@@ -1,9 +1,60 @@
-export default function CategoryTree({className}) {
+import { useParams, useNavigate } from "react-router-dom";
+import { getAllChildrenOfCategory } from "services/apiRequestsShop";
+import { useEffect, useState } from "react";
+import { API_CATEGORY_URL } from "config";
+import "./CategoryTree.css";
 
+export default function CategoryTree({ className }) {
+  const params = useParams();
+  const [categoryChildren, setCategoryChildren] = useState([]);
+  const [parent, setParent] = useState({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCategoryChildren = async () => {
+      try {
+        const response = await getAllChildrenOfCategory(params.slug);
+        console.log(response.children);
+        setCategoryChildren(response.data.children);
+        setParent(response.data.parent);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchCategoryChildren();
+  }, [params]);
+
+  const handleNavigationClick = (slug, event) => {
+    event.stopPropagation();
+    navigate(`${API_CATEGORY_URL}/${slug}`);
+  };
+
+  const listCategoryChildren = () => {
+    return (
+      <>
+        {categoryChildren.map((child) => (
+          <p key={child.slug} onClick={(event) => handleNavigationClick(child.slug, event)} className="category-child">
+            {child.name}
+          </p>
+        ))}
+      </>
+    );
+  };
 
   return (
-  <div className={`${className}`}>
-    Category Tree TODO
-  </div>
-);
+    <div className={`${className}`}>
+      {parent && parent.slug ? (
+        <p>
+          <span>Go back to </span>
+          <span 
+            onClick={(event) => handleNavigationClick(parent.slug, event)} 
+            className="parent-link"
+          >
+            {parent.name}
+          </span>
+        </p>
+      ) : null}
+      {listCategoryChildren()}
+    </div>
+  );
 }
--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -8,8 +8,8 @@ export default function Category() {
     <div className='category-view-container d-flex flex-column mx-auto gap-3'>
       <CategoryTopBar className="bg-secondary mt-3"/>
       <div className='d-flex flex-row gap-3'>
-        <CategoryTree className="bg-secondary category-tree-basis"/>
-        <ProductList className="bg-secondary product-list-basis"/>
+        <CategoryTree className="bg-secondary category-tree-basis p-2"/>
+        <ProductList className="bg-secondary product-list-basis p-2"/>
       </div>
     </div>
   );
