0e2d225b734ac41033e44eb7f28ecd194e686c3d
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-04 17:35:11 +0200

    update styling

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -6,7 +6,7 @@ import "./Category.css";
 export default function Category() {
   return (
     <div className='category-view-container d-flex flex-column mx-auto gap-3'>
-      <CategoryTopBar className="bg-secondary mt-3"/>
+      <CategoryTopBar className="bg-secondary mt-3 p-2"/>
       <div className='d-flex flex-row gap-3'>
         <CategoryTree className="bg-secondary category-tree-basis p-2"/>
         <ProductList className="bg-secondary product-list-basis p-2"/>
