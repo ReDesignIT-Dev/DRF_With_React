cd5c55473e0de34bc3198522e56da1e1caec79b6
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-05 18:33:00 +0200

    upgrade list of products to send to product page

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { getAllProductsInCategory } from "services/apiRequestsShop";
 import "./ProductList.css";
 import useQueryParams from "hooks/useQueryParams";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { API_PRODUCT_URL } from "config";
 
-export default function ProductList({className}) {
+export default function ProductList({ className }) {
   const params = useParams();
   const queryParams = useQueryParams();
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,15 +23,25 @@ export default function ProductList({className}) {
     fetchProducts();
   }, [params, queryParams]);
 
+  const handleNavigationClick = (slug, event) => {
+    event.stopPropagation();
+    navigate(`${API_PRODUCT_URL}/${slug}`);
+  };
+
   const listProducts = () => {
     return (
       <>
         {products.map((product) => (
-          <div key={product.slug} className='single-product-on-list d-flex flex-row w-100'>
+          <div
+            key={product.slug}
+            className='single-product-on-list d-flex flex-row w-100'
+            role='button'
+            onClick={(event) => handleNavigationClick(product.slug, event)}
+          >
             <img src={product.image} alt={product.name}></img>
             <div className='product-details d-flex justify-content-between w-100'>
               <h2>{product.name}</h2>
-              <p className="product-price">{product.price} PLN</p>
+              <p className='product-price'>{product.price} PLN</p>
             </div>
           </div>
         ))}
@@ -37,5 +49,9 @@ export default function ProductList({className}) {
     );
   };
 
-  return <div className={`product-list-container d-flex flex-column gap-3 w-100 p-3 ${className}`}>{listProducts()}</div>;
+  return (
+    <div className={`product-list-container d-flex flex-column gap-3 w-100 p-3 ${className}`}>
+      {listProducts()}
+    </div>
+  );
 }
