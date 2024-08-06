40a4dca3a0330c4c54df8e2e01bf7f48bb0d1947
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-06 08:14:41 +0200

    styling and grouping in product page

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -1,39 +1,46 @@
 import { getProduct } from "services/apiRequestsShop";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./Product.css";
 
 export default function Product() {
-    const params = useParams();
-    const [product, setProduct] = useState({
-        name: "",
-        category: null,
-        description: "",
-        price: "",
-        sale_start: null,
-        sale_end: null,
-        is_on_sale: false,
-        image: "",
-        slug: ""
-      });
+  const params = useParams();
+  const [product, setProduct] = useState({
+    name: "",
+    category: null,
+    description: "",
+    price: "",
+    sale_start: null,
+    sale_end: null,
+    is_on_sale: false,
+    image: "",
+    slug: "",
+  });
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-          try {
-            const response = await getProduct(params.slug);
-            setProduct(response.data);
-          } catch (error) {
-            console.error("Error fetching products:", error);
-          }
-        };
-        fetchProduct();
-      }, [params]);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await getProduct(params.slug);
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProduct();
+  }, [params]);
 
-    return (
-      <div className='product-view-container'>
-        Product:
-        {product.name}
-        {product.image}
-        {product.price}
+  return (
+    <div className='product-view-container d-flex flex-column mx-auto'>
+      <div className='product-top-info d-flex flex-row'>
+        <div className='product-images'>
+          <img src={product.image} alt={product.name} />
+        </div>
+        <div className="product-cart-info d-flex flex-column mx-auto">
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+        </div>
       </div>
-    );
-  }
+      <div className='product-description'>{product.description}</div>
+    </div>
+  );
+}
