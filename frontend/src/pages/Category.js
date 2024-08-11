a407361b6b1bae13bfd56f5fbca9598358918ab8
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-11 09:11:36 +0200

    upgrade bootstrap so the columns are not the same height

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -7,9 +7,9 @@ export default function Category() {
   return (
     <div className='category-view-container d-flex flex-column mx-auto gap-3'>
       <CategoryTopBar className="bg-secondary mt-3 p-2"/>
-      <div className='d-flex flex-row gap-3'>
+      <div className='d-flex align-items-start gap-3'>
         <CategoryTree className="bg-secondary category-tree-basis p-2"/>
-        <ProductList className="bg-secondary product-list-basis p-2"/>
+        <ProductList className="bg-secondary product-list-basis p-2 flex-grow-1"/>
       </div>
     </div>
   );
