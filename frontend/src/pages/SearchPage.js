7685aa1a0acad13d48a8e7b9997887c18cd97fd0
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-11 09:11:16 +0200

    upgrade bootstrap classes so the column is not the same height

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -4,12 +4,11 @@ import CategoryAssociatedTree from "components/CategoryAssociatedTree";
 import "./SearchPage.css";
 
 export default function SearchPage() {
-
   return (
-    <div className='searchpage-view-container d-flex flex-column mx-auto gap-3'>
-      <div className='d-flex flex-row gap-3'>
+    <div className='searchpage-view-container mx-auto gap-3'>
+      <div className='d-flex gap-3 mt-3 align-items-start'>
         <CategoryAssociatedTree className="bg-secondary category-tree-basis p-2"/>
-        <ProductSearchList className="bg-secondary product-list-basis p-2"/>
+        <ProductSearchList className="bg-secondary product-list-basis p-2 flex-grow-1"/>
       </div>
     </div>
   );
