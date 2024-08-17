2c4ee89aca09ffa5b7e548ed671f7f599567921e
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-17 13:04:50 +0200

    fix backend urls for user

--- api/users/urls.py
+++ api/users/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('password-reset/', UserPasswordResetView.as_view(), name='password-reset'),
     path('password-reset/<str:token>/', UserPasswordResetActivationView.as_view(),
          name='password-reset-activation'),
-    path('login/', LoginView.as_view(), name='api-login'),
-    path('logout/', LogoutView.as_view(), name='api-logout'),
-    path('logout-all/', LogoutAllView.as_view(), name='api-logout-all'),
-    path('validate-token/', ValidateToken.as_view(), name='validate-token'),
+    path('login', LoginView.as_view(), name='api-login'),
+    path('logout', LogoutView.as_view(), name='api-logout'),
+    path('logout-all', LogoutAllView.as_view(), name='api-logout-all'),
+    path('validate-token', ValidateToken.as_view(), name='validate-token'),
 ]
