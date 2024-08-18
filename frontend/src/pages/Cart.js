b982cef4a82a516d3abbe7b90a4834f40003dc57
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-18 12:30:06 +0200

    working base cart summary

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -4,7 +4,7 @@ import Loading from "components/Loading";
 import "./Cart.css";
 
 export default function Cart() {
-  const [items, setItems] = useState([]);
+  const [cartData, setCartData] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [total, setTotal] = useState(0);
@@ -15,7 +15,7 @@ export default function Cart() {
         setLoading(true);
         setTotal(1);
         const response = await getCart();
-        console.log(response.data);
+        setCartData(response.data);
       } catch (error) {
         console.error("Error fetching products:", error);
       } finally {
@@ -28,10 +28,10 @@ export default function Cart() {
   const handleQuantityChange = async (itemId, quantity) => {
     try {
       const response = await updateCartItemQuantity(itemId, quantity);
-      const updatedItems = items.map((item) =>
+      const updatedItems = cartData.items.map((item) =>
         item.id === itemId ? { ...item, quantity: response.data.quantity } : item
       );
-      setItems(updatedItems);
+      setCartData(updatedItems);
       calculateTotal();
     } catch (error) {
       setError(error.message);
@@ -49,7 +49,7 @@ export default function Cart() {
   };
 
   const calculateTotal = () => {
-    const totalAmount = items.reduce((sum, item) => sum + item.quantity * item.price, 0);
+    const totalAmount = cartData.items.reduce((sum, item) => sum + item.quantity * item.price, 0);
     setTotal(totalAmount.toFixed(2));
   };
 
@@ -60,37 +60,53 @@ export default function Cart() {
   if (error) {
     return <div>{error}</div>;
   }
+  const cartItemList = () => (
+    <>
+      {cartData.items.map((item) => (
+        <div key={item.product.slug} className='basket-item'>
+          <img src={item.product.image} alt={item.product.name} />
+          <div>
+            <h4>{item.product.name}</h4>
+            <p>Price: ${item.price}</p>
+            <p>
+              Quantity:
+              <input
+                type='number'
+                value={item.quantity}
+                onChange={(e) =>
+                  handleQuantityChange(item.product.slug, parseInt(e.target.value, 10))
+                }
+                min='1'
+              />
+            </p>
+            <button onClick={() => handleRemoveItem(item.product.slug)}>Remove</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
-    <div>
+    <div className="cart-container mx-auto">
       <h2>Your Basket</h2>
-      {items.length === 0 ? (
+      {cartData.items.length === 0 ? (
         <p>Your basket is empty.</p>
       ) : (
-        <div>
-          {items.map((item) => (
-            <div key={item.id} className='basket-item'>
-              <img src={item.product.image_url} alt={item.product.name} />
-              <div>
-                <h4>{item.product.name}</h4>
-                <p>Price: ${item.price}</p>
-                <p>
-                  Quantity:
-                  <input
-                    type='number'
-                    value={item.quantity}
-                    onChange={(e) => handleQuantityChange(item.slug, e.target.value)}
-                    min='1'
-                  />
-                </p>
-                <button onClick={() => handleRemoveItem(item.slug)}>Remove</button>
-              </div>
-            </div>
-          ))}
-          <div className='basket-total'>
-            <h3>Total: ${total}</h3>
+        <div className='d-flex flex-row'>
+          <div className='cart-item-list'>{cartItemList()}</div>
+          <div className='cart-summary d-flex flex-column'>
+            <div className="cart-payment"><h3>Total: ${total}</h3></div>
+            <div><button className='checkout-button'>Proceed to Checkout</button></div>
           </div>
-          <button className='checkout-button'>Proceed to Checkout</button>
+          
         </div>
       )}
     </div>
