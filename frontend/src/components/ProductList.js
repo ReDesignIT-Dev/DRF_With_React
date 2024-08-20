980716cc80da01c650f73886a7e3884f8697dbdd
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-20 18:56:01 +0200

    add confirmation if item is added to cart

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -4,12 +4,15 @@ import "./ProductList.css";
 import useQueryParams from "hooks/useQueryParams";
 import { useParams, useNavigate } from "react-router-dom";
 import { API_PRODUCT_URL } from "config";
+import { addToCart } from "services/apiRequestsShop";
 
 export default function ProductList({ className }) {
   const params = useParams();
   const queryParams = useQueryParams();
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
+  const [confirmationMessage, setConfirmationMessage] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -28,6 +31,18 @@ export default function ProductList({ className }) {
     navigate(`${API_PRODUCT_URL}/${slug}`);
   };
 
+  const handleAddToCartClick = async (product, event) => {
+    event.stopPropagation();
+    try {
+      await addToCart(product.slug, 1);
+      setConfirmationMessage(`${product.name} was added to the cart!`);
+      setShowConfirmation(true);
+      setTimeout(() => setShowConfirmation(false), 3000); // Hide after 3 seconds
+    } catch (error) {
+      console.error("Error adding product to cart:", error);
+    }
+  };
+
   const listProducts = () => {
     return (
       <>
@@ -41,7 +56,15 @@ export default function ProductList({ className }) {
             <img src={product.image} alt={product.name}></img>
             <div className='product-details d-flex justify-content-between w-100'>
               <h2>{product.name}</h2>
-              <p className='product-price'>{product.price} PLN</p>
+              <div className='product-price-and-cart d-flex align-items-center'>
+                <p className='product-price'>{product.price} PLN</p>
+                <button
+                  className='product-add-to-cart-btn'
+                  onClick={(event) => handleAddToCartClick(product, event)}
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
           </div>
         ))}
@@ -51,6 +74,7 @@ export default function ProductList({ className }) {
 
   return (
     <div className={`product-list-container d-flex flex-column gap-3 w-100 p-3 ${className}`}>
+      {showConfirmation && <div className='confirmation-message'>{confirmationMessage}</div>}
       {listProducts()}
     </div>
   );
