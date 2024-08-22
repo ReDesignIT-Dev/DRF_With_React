23c013fdb2a6d81fa5cbc388eb852f3493d8e305
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-22 21:27:58 +0200

    upgrade login/signup form popup to work with already created components

--- frontend/src/components/LoginForm/ErrorLabel.js
+++
@@ -1,5 +0,0 @@
-const ErrorLabel = ({ error, groupState, state }) => (
-    <label className={`${groupState === state ? "" : "hidden"} errorLabel d-flex justify-content-center text-center px-2`} >{error}</label>
-  );
-
-  export default ErrorLabel;
