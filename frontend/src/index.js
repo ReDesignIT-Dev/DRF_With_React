98ec6d9b189e9b28dce96003b954958719a96cd3
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-27 20:55:00 +0200

    dir naming change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import store from "./reduxComponents/store";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
