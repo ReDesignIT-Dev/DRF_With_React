7c2e5ce288b257b7dde334f5f723ccb9b2b9b85b
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-08-31 19:13:24 +0200

    create global auth for all components

--- frontend/src/components/SignInButton.js
+++ frontend/src/components/SignInButton.js
@@ -2,19 +2,25 @@ import React from "react";
 import "./SignInButton.css";
 import "react-bootstrap";
 import { FaRegUserCircle } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { logout } from "reduxComponents/reducers/authReducer";
+import { useAuth } from "hooks/useAuth";
 
-export default function SignInButton({ isLoggedIn, handleLogout, handleLoginClick }) {
+export default function SignInButton({handleIconClick}) {
+  const isLoggedIn = useAuth();
+  const dispatch = useDispatch();
+
   const handleClick = () => {
     if (isLoggedIn) {
-      handleLogout();
+      dispatch(logout());
     } else {
-      handleLoginClick();
+      handleIconClick();
     }
   };
 
   return (
     <div onClick={handleClick} className='loginicon'>
-      {isLoggedIn ? (<p className="my-auto px-3">Log Out</p>) : <FaRegUserCircle size={'1x'} />}
+      {isLoggedIn ? <p className='my-auto px-3'>Log Out</p> : <FaRegUserCircle size={"1x"} />}
     </div>
   );
 }
