c81c03daf13ff32feceda23433d847c6de8b9c5e
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-09-01 10:37:12 +0200

    update to typescript

---
+++ frontend/src/pages/Category.tsx
@@ -0,0 +1,55 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { validateIfCategoryExists } from "services/apiRequestsShop";
+import ProductList from "components/ProductList";
+import CategoryTree from "components/CategoryTree";
+import CategoryTopBar from "components/CategoryTopBar";
+import "./Category.css";
+import NotFound from "./NotFound";
+
+
+export default function Category() {
+  const params = useParams<Record<string, string | undefined>>(); // Use the correct type for useParams
+  const [isValidCategory, setIsValidCategory] = useState<boolean>(false);
+  const [categoryNotFound, setCategoryNotFound] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkCategory = async () => {
+      try {
+        if (params.slug) {
+          const exists = await validateIfCategoryExists(params.slug);
+          if (exists) {
+            setIsValidCategory(true);
+          } else {
+            setCategoryNotFound(true);
+          }
+        } else {
+          setCategoryNotFound(true); // Handle the case where `slug` is not present
+        }
+      } catch (error) {
+        console.error("Error checking category:", error);
+        setCategoryNotFound(true);
+      }
+    };
+
+    checkCategory();
+  }, [params.slug]); // Dependency array should only include `params.slug`
+
+  if (categoryNotFound) {
+    return <NotFound />;
+  }
+
+  return (
+    <>
+      {isValidCategory && (
+        <div className='category-view-container d-flex flex-column mx-auto gap-3'>
+          <CategoryTopBar className="bg-secondary mt-3 p-2" />
+          <div className='d-flex align-items-start gap-3'>
+            <CategoryTree className="bg-secondary category-tree-basis p-2" />
+            <ProductList className="bg-secondary product-list-basis p-2 flex-grow-1" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
