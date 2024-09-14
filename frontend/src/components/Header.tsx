1e2be4490e330d0c1cd259124f0db2bfd2b734cf
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-09-14 12:51:01 +0200

    move auth checking out from header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import LoginFormPop from "./LoginFormPop";
+import React from "react";
 import SignInButton from "./SignInButton";
 import SearchBox from "./SearchBox";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,66 +6,51 @@ import "./Header.scss";
 import LogoRacoon from "./LogoRacoon";
 import CategoryDropdown from "./CategoryDropdown";
 import { FaShoppingCart } from "react-icons/fa";
-import { useAuth } from "hooks/useAuth";
 
 const Header: React.FC = () => {
-  const [shopPopupLoginRegister, setShowPopupLoginRegister] = useState<boolean>(true);
   const navigate = useNavigate();
-  const isLoggedIn = useAuth();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      setShowPopupLoginRegister(true);
-    }
-  }, [isLoggedIn]);
-
-  const handleLoginClick = () => {
-    setShowPopupLoginRegister((isShowLogin: boolean) => !isShowLogin);
-  };
 
   return (
-    <div className='main-header px-3'>
-      <div className='header d-flex flex-column py-2'>
-        <div className='top-header d-flex gap-2 justify-content-center align-items-center'>
-          <div className='header-logo d-flex justify-content-center align-items-center'>
-            <Link to='/'>
+    <div className="main-header px-3">
+      <div className="header d-flex flex-column py-2">
+        <div className="top-header d-flex gap-2 justify-content-center align-items-center">
+          <div className="header-logo d-flex justify-content-center align-items-center">
+            <Link to="/">
               <LogoRacoon />
             </Link>
           </div>
-          <div className='header-searchbar'>
+          <div className="header-searchbar">
             <SearchBox />
           </div>
 
-          <div className='header-signin d-flex flex-row gap-2 justify-content-center align-items-center'>
+          <div className="header-signin d-flex flex-row gap-2 justify-content-center align-items-center">
             <div
-              className='shopping-cart-icon d-flex justify-content-center align-items-center'
+              className="shopping-cart-icon d-flex justify-content-center align-items-center"
               onClick={() => navigate("/cart")}
             >
-              <FaShoppingCart size={'1x'} />
+              <FaShoppingCart size={"1x"} />
             </div>
-            <SignInButton handleIconClick={handleLoginClick} />
+            <SignInButton />
           </div>
         </div>
 
-        <div className='header-nav d-flex flex-row gap-2 my-2'>
-          <div className='nav-element'>
+        <div className="header-nav d-flex flex-row gap-2 my-2">
+          <div className="nav-element">
             <CategoryDropdown />
           </div>
-          <div className='nav-element'>
-            <Link to='/'> Home </Link>
+          <div className="nav-element">
+            <Link to="/"> Home </Link>
           </div>
-          <div className='nav-element'>
-            <Link to='/contact'> Contact </Link>
+          <div className="nav-element">
+            <Link to="/contact"> Contact </Link>
           </div>
-          <div className='nav-element'>
-            <Link to='/about'> About </Link>
+          <div className="nav-element">
+            <Link to="/about"> About </Link>
           </div>
-          <div className='nav-element'>
-            <Link to='/about/history'> History </Link>
+          <div className="nav-element">
+            <Link to="/about/history"> History </Link>
           </div>
         </div>
-
-        <LoginFormPop isShowLogin={shopPopupLoginRegister} handleXClick={handleLoginClick} />
       </div>
     </div>
   );
