05bc139a89f1e4d5a94a5682cb0aec84c2d7caf5
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-11-07 15:41:27 +0100

    add shop footer and header based on main one

---
+++ frontend/src/components/ShopFooter.tsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+import React from "react";
+
+const ShopFooter: React.FC = () => {
+  return (
+    <div className="footer">
+      TODO FOOTER
+    </div>
+  );
+}
+
+export default ShopFooter;
---
+++ frontend/src/components/ShopHeader.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import SignInButton from "./SignInButton";
+import SearchBox from "./SearchBox";
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.scss";
+import LogoRacoon from "./LogoRacoon";
+import CategoryDropdown from "./CategoryDropdown";
+import { FaShoppingCart } from "react-icons/fa";
+import { FRONTEND_CART_URL, FRONTEND_SHOP_URL } from "config";
+
+const ShopHeader: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="main-header px-3">
+      <div className="header d-flex flex-column py-2">
+        <div className="top-header d-flex gap-2 justify-content-center align-items-center">
+          <div className="header-logo d-flex justify-content-center align-items-center">
+            <Link to="/">
+              <LogoRacoon />
+            </Link>
+          </div>
+          <div className="header-searchbar">
+            <SearchBox />
+          </div>
+
+          <div className="header-signin d-flex flex-row gap-2 justify-content-center align-items-center">
+            <Link
+              to={`${FRONTEND_SHOP_URL}${FRONTEND_CART_URL}`}
+              className="shopping-cart-icon d-flex justify-content-center align-items-center"
+            >
+              <FaShoppingCart size={"40px"} />
+            </Link>
+            <SignInButton />
+          </div>
+        </div>
+
+        <div className="header-nav d-flex flex-row gap-2 my-2">
+          <div className="nav-element">
+            <CategoryDropdown />
+          </div>
+          <div className="nav-element">
+            <Link to="/"> Home </Link>
+          </div>
+          <div className="nav-element">
+            <Link to="/contact"> Contact </Link>
+          </div>
+          <div className="nav-element">
+            <Link to="/about"> About </Link>
+          </div>
+          <div className="nav-element">
+            <Link to="/about/history"> History </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopHeader;
