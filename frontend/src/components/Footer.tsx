14cfba03649afee8e08f0895525115686a993aba
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-11-11 09:42:40 +0100

    update home main footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,29 @@
 import "./Footer.css";
-import React from "react";
-
+import { Box, Typography, Link } from "@mui/material";
 const Footer: React.FC = () => {
   return (
-    <div className="footer">
-      TODO FOOTER
-    </div>
+    <Box
+      sx={{
+        backgroundColor: "#333",
+        color: "#fff",
+        py: 3,
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="body2">
+        © 2024 ReDesignIT. All rights reserved.
+      </Typography>
+      <Link href="#" color="inherit" underline="hover" sx={{ mx: 1 }}>
+        LinkedIn
+      </Link>
+      <Link href="#" color="inherit" underline="hover" sx={{ mx: 1 }}>
+        GitHub
+      </Link>
+      <Link href="#" color="inherit" underline="hover" sx={{ mx: 1 }}>
+        YouTube
+      </Link>
+    </Box>
   );
-}
+};
 
 export default Footer;
