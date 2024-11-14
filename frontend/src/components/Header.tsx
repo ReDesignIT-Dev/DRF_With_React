c3db5129f48637a2fd97976e9b0ff8fb249fced7
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-11-14 21:58:54 +0100

    modify header for main page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,58 +1,37 @@
 import React from "react";
-import SignInButton from "./SignInButton";
-import SearchBox from "./SearchBox";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Header.scss";
-import LogoRacoon from "./LogoRacoon";
-import CategoryDropdown from "./CategoryDropdown";
-import { FaShoppingCart } from "react-icons/fa";
+
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import { FRONTEND_ABOUT_URL, FRONTEND_CONTACT_URL } from "config";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="main-header px-3">
-      <div className="header d-flex flex-column py-2">
-        <div className="top-header d-flex gap-2 justify-content-center align-items-center">
-          <div className="header-logo d-flex justify-content-center align-items-center">
-            <Link to="/">
-              <LogoRacoon />
-            </Link>
-          </div>
-          <div className="header-searchbar">
-            <SearchBox />
-          </div>
-
-          <div className="header-signin d-flex flex-row gap-2 justify-content-center align-items-center">
-            <div
-              className="shopping-cart-icon d-flex justify-content-center align-items-center"
-              onClick={() => navigate("/cart")}
-            >
-              <FaShoppingCart size={"40px"} />
-            </div>
-            <SignInButton />
-          </div>
-        </div>
-
-        <div className="header-nav d-flex flex-row gap-2 my-2">
-          <div className="nav-element">
-            <CategoryDropdown />
-          </div>
-          <div className="nav-element">
-            <Link to="/"> Home </Link>
-          </div>
-          <div className="nav-element">
-            <Link to="/contact"> Contact </Link>
-          </div>
-          <div className="nav-element">
-            <Link to="/about"> About </Link>
-          </div>
-          <div className="nav-element">
-            <Link to="/about/history"> History </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <AppBar position="static" sx={{ backgroundColor: "#333" }}>
+    <Toolbar>
+    <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1, padding: 1 }}>
+          <img
+            src="logo-redesign.jpg" // Replace with your actual path, e.g., /images/logo.png
+            alt="ReDesignIT Logo"
+            style={{ height: "60px", marginRight: "10px", borderRadius: 10 }} // Adjust height and spacing as needed
+          />
+      <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        ReDesignIT
+      </Typography>
+      </Box>
+      <Button color="inherit" onClick={() => navigate("/")}>Home</Button>
+      <Button color="inherit" onClick={() => navigate(FRONTEND_ABOUT_URL)}>About Me</Button>
+      <Button color="inherit" onClick={() => navigate(FRONTEND_CONTACT_URL)}>Contact</Button>
+    </Toolbar>
+  </AppBar>
   );
 };
 
