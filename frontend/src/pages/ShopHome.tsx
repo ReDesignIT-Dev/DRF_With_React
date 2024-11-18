db02d17b915425b330584c8ab27ffad72168e6c1
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-11-18 16:40:16 +0100

    add shop home page

---
+++ frontend/src/pages/ShopHome.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchCategories } from "../reduxComponents/reducers/categoryReducer";
+import { RootState, AppDispatch } from "../reduxComponents/store";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid2,
+  Typography,
+  Container,
+  Box,
+} from "@mui/material";
+import { FRONTEND_SHOP_URL, FRONTEND_CATEGORY_URL } from "config";
+
+export default function ShopHome() {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { categories, isLoading, error } = useSelector(
+    (state: RootState) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  const handleCategoryClick = (slug: string) => {
+    navigate(`${FRONTEND_SHOP_URL}${FRONTEND_CATEGORY_URL}/${slug}`);
+  };
+
+  const renderCategoryTree = (categories: Category[]) => (
+    <>
+      {categories.map((category) => (
+        <Grid2 container key={category.slug} sx={{ display: "flex", flexDirection: "column" }} gap={5}>
+          <Card>
+            <CardActionArea onClick={() => handleCategoryClick(category.slug)}>
+              <CardContent sx={{ textAlign: "center" }}>
+                <Typography variant="h6">{category.name}</Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+
+          {category.children && category.children.length > 0 && (
+            <Grid2
+              container
+              justifyContent="center"
+              sx={{ display: "flex", flexDirection: "row" }} gap={5}
+            >
+              {renderCategoryTree(category.children)}
+            </Grid2>
+          )}
+        </Grid2>
+      ))}
+    </>
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading categories.</p>;
+
+  return (
+    <Container>
+      <Typography variant="h4" align="center" gutterBottom>
+        Welcome to Our Shop
+      </Typography>
+      <Box mt={4}>
+        {categories.length > 0 ? (
+          renderCategoryTree(categories)
+        ) : (
+          <p>No categories available.</p>
+        )}
+      </Box>
+    </Container>
+  );
+}
