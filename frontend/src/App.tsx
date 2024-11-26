8f4c6607b8a924d4da9461c14c1216df0da5f940
Arek <arkadiusz.budkowski.dev@gmail.com>
2024-11-26 21:39:47 +0100

    separate app to home and shop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-
-import Home from "pages/Home";
 import Shop from "pages/Shop";
-import Header from "components/Header";
-import Footer from "components/Footer";
 import NotFound from "pages/NotFound";
 import { FRONTEND_SHOP_URL } from "config";
+import MainPage from "pages/MainPage";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path={`${FRONTEND_SHOP_URL}/*`} element={<Shop />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<MainPage />} />
+          <Route path={`${FRONTEND_SHOP_URL}/*`} element={<Shop />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Box>
   );
 }
 
