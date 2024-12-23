2bf34f8395b2456c76e6d8b5ec11423afec0d087
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2024-12-23 10:02:21 +0100

    update shop pre url

--- DRF_React/urls.py
+++ DRF_React/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.shop.urls')),
+    path('shop/', include('api.shop.urls')),
     path('', include('api.users.urls')),
     path('home/', include('api.home.urls')),
     re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico', permanent=True))
