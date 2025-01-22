b264b332764396710087c32f164fd3aa334d1f02
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-01-22 17:41:46 +0100

    create helper function to find id in slug

---
+++ frontend/src/utils/utils.ts
@@ -0,0 +1,5 @@
+export function extractIdFromSlug(slug: string): number | null {
+    const idMatch = slug.match(/(\d+)$/); // Matches the last group of digits in the slug
+    return idMatch ? parseInt(idMatch[1], 10) : null;
+  }
+  
