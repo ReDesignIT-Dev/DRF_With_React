f90f8e97880e1dc43feb0e9da1b7b9b5cf917cc6
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-02-03 20:42:26 +0100

    delete too many requests for backend to use one call for categories

--- api/shop/urls.py
+++ api/shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ProductList, ProductCreate, ProductEditView, HomeView, ProductView, CategoryTreeView, \
-    CategoryView, CategoryCreateView, CategoryEditView, ShopAdminPanel, ShopAdminPanelProducts, CategoryChildrenView, \
-    CategoryProductsView, CategoryParentsView, ProductParentCategoryView, ProductSearchView, \
+    CategoryView, CategoryCreateView, CategoryEditView, ShopAdminPanel, ShopAdminPanelProducts, \
+    CategoryProductsView, ProductSearchView, \
     CategoriesSearchAssociatedView, ShoppingCartItemCreateView, \
     ShoppingCartItemUpdateView, ShoppingCartItemDestroyView, ShoppingCartItemListView, CategoryFlatView
 
@@ -13,15 +13,12 @@ urlpatterns = [
     path('product/create', ProductCreate.as_view(), name='product_create'),
     path('product/<int:id>', ProductView.as_view(), name='product'),
     path('product/<int:id>/edit', ProductEditView.as_view(), name='product_edit'),
-    path('product/<int:id>/parent-category', ProductParentCategoryView.as_view(), name='parent-category-detail'),
     path('category/tree', CategoryTreeView.as_view(), name='tree_categories'),
     path('category/flat', CategoryFlatView.as_view(), name='flat_categories'),
     path('category/create', CategoryCreateView.as_view(), name='category_create'),
     path('category/<int:id>', CategoryView.as_view(), name='category'),
     path('category/<int:id>/edit', CategoryEditView.as_view(), name='category_edit'),
-    path('category/<int:id>/children', CategoryChildrenView.as_view(), name='category_children'),
     path('category/<int:id>/products', CategoryProductsView.as_view(), name='category_products'),
-    path('category/<int:id>/parents', CategoryParentsView.as_view(), name='category_parents'),
     path('shopping-admin-panel', ShopAdminPanel.as_view(), name='shop_admin_panel'),
     path('shopping-admin-panel/products', ShopAdminPanelProducts.as_view(), name='shop_admin_panel_products'),
     path('cart', ShoppingCartItemListView.as_view(), name='shopping-cart'),
