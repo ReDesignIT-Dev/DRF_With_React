8f83f7981ca7195130d2a804671967536c5beb8b
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-02-10 19:28:27 +0100

    update naming

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,4 +1,4 @@
-export function extractIdFromSlug(slug: string): number | null {
+export function getIdFromSlug(slug: string): number | null {
     const idMatch = slug.match(/(\d+)$/); // Matches the last group of digits in the slug
     return idMatch ? parseInt(idMatch[1], 10) : null;
   }
