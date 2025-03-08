662c148f19e47b29d031f0cdb8164d82e28753eb
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-03-08 10:11:50 +0100

    simplify search page components

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -7,8 +7,8 @@ export default function SearchPage() {
   return (
     <div className='searchpage-view-container mx-auto gap-3'>
       <div className='d-flex gap-3 mt-3 align-items-start'>
-        <CategoryAssociatedTree className="bg-secondary category-tree-basis p-2"/>
-        <ProductSearchList className="bg-secondary product-list-basis p-2 flex-grow-1"/>
+        <CategoryAssociatedTree />
+        <ProductSearchList />
       </div>
     </div>
   );
