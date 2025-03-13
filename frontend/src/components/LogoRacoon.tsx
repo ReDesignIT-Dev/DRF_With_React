549fb7ec0cc3ca255b5a5d4d884d84086e8b12f4
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-03-13 20:30:48 +0100

    upgrade logo

--- frontend/src/components/LogoRacoon.tsx
+++ frontend/src/components/LogoRacoon.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import racoonLogo from 'assets/images/racoon-logo.png';
+import React from "react";
+import racoonLogo from "assets/images/racoon-logo.png";
+import { Box } from "@mui/material";
 
 const LogoRacoon: React.FC = () => {
   return (
-    <div>
-      <img src={racoonLogo} alt="Logo" />
-    </div>
+    <Box
+      sx={{
+        width: 60,
+        height: 60,
+        minWidth: 60,
+        minHeight: 60,
+        marginRight: "auto",
+        borderRadius: "5px",
+        overflow: "hidden",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src={racoonLogo}
+        alt="Logo"
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "contain",
+        }}
+      />
+    </Box>
   );
 };
 
