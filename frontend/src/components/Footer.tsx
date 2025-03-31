22c2ea754487442d5e5b4951aea8d383f342579f
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-03-31 17:05:22 +0200

    upgrade footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,29 +8,43 @@ const Footer: React.FC = () => {
       sx={{
         backgroundColor: "#333",
         color: "#fff",
-        py: 3,
-        px: 2,
+        py: 2,
+        px: 6,
         display: "flex",
         alignItems: "center",
+        mt: 2,
       }}
     >
-      {/* Empty box as a spacer to center the text */}
-      <Box sx={{ flex: 1 }} />
-
-      {/* Centered Typography */}
       <Typography variant="body2" sx={{ textAlign: "center", flex: 1 }}>
         © 2024 ReDesignIT. All rights reserved.
       </Typography>
-
-      {/* Icons on the right */}
-      <Box sx={{ display: "flex", gap: 2, flex: 1, justifyContent: "flex-end" }}>
-        <Link href="https://www.linkedin.com/in/arkadiusz-budkowski/" color="inherit" underline="none">
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          justifyContent: "flex-end",
+          flexShrink: 0,
+        }}
+      >
+        <Link
+          href="https://www.linkedin.com/in/arkadiusz-budkowski/"
+          color="inherit"
+          underline="none"
+        >
           <LinkedIn fontSize="large" />
         </Link>
-        <Link href="https://github.com/Hamster-Inside" color="inherit" underline="none">
+        <Link
+          href="https://github.com/ReDesignIT-Dev"
+          color="inherit"
+          underline="none"
+        >
           <GitHub fontSize="large" />
         </Link>
-        <Link href="https://www.youtube.com/@ReDesignIT" color="inherit" underline="none">
+        <Link
+          href="https://www.youtube.com/@ReDesignIT"
+          color="inherit"
+          underline="none"
+        >
           <YouTube fontSize="large" />
         </Link>
       </Box>
