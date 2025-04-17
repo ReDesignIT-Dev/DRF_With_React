6eff308c49d3ba3fa168d9a7faabbe7b54c6fea7
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-04-17 23:03:41 +0200

    add the totalpages to be returned with the pagination

--- api/shop/views.py
+++ api/shop/views.py
@@ -1,3 +1,5 @@
+import math
+
 from knox.auth import TokenAuthentication
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import (
@@ -206,6 +208,16 @@ class CategoryView(APIView):
 
 class TenPerPagePagination(PageNumberPagination):
     page_size = 10
+
+    def get_paginated_response(self, data):
+        total_pages = math.ceil(self.page.paginator.count / self.page_size)
+        return Response({
+            'count': self.page.paginator.count,
+            'totalPages': total_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class CategoryProductsView(RetrieveAPIView):
