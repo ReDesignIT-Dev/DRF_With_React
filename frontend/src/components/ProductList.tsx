8dc8f6dd0336ef8411fb7ffcdac6f679b6e41782
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-05-13 22:04:02 +0200

    update styling

--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -1,18 +1,15 @@
 import { MouseEvent } from "react";
+import { generatePath, useNavigate } from "react-router-dom";
 import { useCart } from "services/shopServices/cartLogic";
 import "./ProductList.css";
-import { useNavigate, generatePath } from "react-router-dom";
 import { FRONTEND_PRODUCT_URL } from "config";
 import shopDefaultImage from "assets/images/shop_default_image.jpg";
+import { Box } from "@mui/material";
 
-interface ProductListProps {
-  products: Product[];
-}
-
-export default function ProductList({ products }: ProductListProps) {
+export default function ProductList({ products }: {products: Product[]}) {
   const navigate = useNavigate();
   const { addToCart } = useCart();
-  
+
   const handleNavigationClick = (slug: string, event: MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
     const productPath = generatePath(FRONTEND_PRODUCT_URL, { slug });
@@ -30,31 +27,36 @@ export default function ProductList({ products }: ProductListProps) {
   };
 
   return (
-    <div className="product-list-container d-flex flex-column gap-3 w-100 p-3">
-      {products.map((product) => {
-        const imageSrc = product.images?.[0]?.src || shopDefaultImage;
-        const imageAlt = product.images?.[0]?.altText || product.name;
+    <Box sx={{ opacity: products.length === 0 ? 0.5 : 1, transition: "opacity 0.3s" }}>
+      <div className="product-list-container d-flex flex-column gap-3 w-100 p-3">
+        {products.map((product) => {
+          const imageSrc = product.images?.[0]?.src || shopDefaultImage;
+          const imageAlt = product.images?.[0]?.altText || product.name;
 
-        return (
-          <div
-            key={product.id}
-            className="single-product-on-list d-flex flex-row w-100"
-            role="button"
-            onClick={(event) => handleNavigationClick(product.slug, event)}
-          >
-            <img src={imageSrc} alt={imageAlt} />
-            <div className="product-details d-flex justify-content-between w-100">
-              <h2>{product.name}</h2>
-              <div className="product-price-and-cart d-flex align-items-center">
-                <p className="product-price">{product.price} PLN</p>
-                <button className="product-add-to-cart-btn" onClick={(event) => handleAddToCartClick(product, event)}>
-                  Add to cart
-                </button>
+          return (
+            <div
+              key={product.id}
+              className="single-product-on-list d-flex flex-row w-100"
+              role="button"
+              onClick={(event) => handleNavigationClick(product.slug, event)}
+            >
+              <img src={imageSrc} alt={imageAlt} />
+              <div className="product-details d-flex justify-content-between w-100">
+                <h2>{product.name}</h2>
+                <div className="product-price-and-cart d-flex align-items-center">
+                  <p className="product-price">{product.price} PLN</p>
+                  <button
+                    className="product-add-to-cart-btn"
+                    onClick={(event) => handleAddToCartClick(product, event)}
+                  >
+                    Add to cart
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </Box>
   );
 }
